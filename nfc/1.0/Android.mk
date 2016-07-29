a48313947e6e6eeaa87b01d795f0c7bc76373a09
Andreas Huber <andih@google.com>
2016-07-29 15:05:03 -0700

    nfc interfaces in the new style.
    Change-Id: I989828d51a2ac1c79906d865b5bdab227457d2e6
    Signed-off-by: Iliyan Malchev <malchev@google.com>

---
+++ nfc/1.0/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.nfc@1.0
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+intermediates := $(local-generated-sources-dir)
+
+GEN := \
+  $(intermediates)/android/hardware/nfc/1.0/types.cpp \
+  $(intermediates)/android/hardware/nfc/1.0/NfcAll.cpp \
+  $(intermediates)/android/hardware/nfc/1.0/NfcClientCallbackAll.cpp \
+
+$(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
+
+$(GEN): PRIVATE_CUSTOM_TOOL = \
+  hidl-gen -o $(PRIVATE_OUTPUT_DIR) android.hardware.nfc@1.0
+
+$(GEN): $(LOCAL_PATH)/types.hal $(LOCAL_PATH)/INfc.hal $(LOCAL_PATH)/INfcClientCallback.hal
+	$(transform-generated-source)
+
+LOCAL_GENERATED_SOURCES += $(GEN)
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
+
+LOCAL_SHARED_LIBRARIES := \
+  libhwbinder \
+  libutils \
+
+include $(BUILD_SHARED_LIBRARY)
