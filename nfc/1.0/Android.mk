81add9c495993f497ec580f4ee5034808b760392
Iliyan Malchev <malchev@google.com>
2016-08-10 07:39:17 -0700

    android.hardware.nfc@1.0: regenerate Android.mk
    hidl-gen \
    	-Lmakefile \
    	-randroid.hardware:hardware/interfaces \
    	android.hardware.nfc@1.0
    
    Change-Id: Ib2d965db93fb248f74ce44efea02b3c4bb2dbe08
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- nfc/1.0/Android.mk
+++ nfc/1.0/Android.mk
@@ -6,27 +6,60 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 intermediates := $(local-generated-sources-dir)
 
-GEN := \
-  $(intermediates)/android/hardware/nfc/1.0/types.cpp \
-  $(intermediates)/android/hardware/nfc/1.0/NfcAll.cpp \
-  $(intermediates)/android/hardware/nfc/1.0/NfcClientCallbackAll.cpp \
-
 HIDL := $(HOST_OUT_EXECUTABLES)/hidl-gen$(HOST_EXECUTABLE_SUFFIX)
 
+#
+# Build types.hal
+#
+GEN := $(intermediates)/android/hardware/nfc/1.0/types.cpp
 $(GEN): $(HIDL)
-
+$(GEN): PRIVATE_HIDL := $(HIDL)
+$(GEN): PRIVATE_DEPS := $(LOCAL_PATH)/types.hal
 $(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
-
 $(GEN): PRIVATE_CUSTOM_TOOL = \
-  $(HIDL) -o $(PRIVATE_OUTPUT_DIR) -r android.hardware:hardware/interfaces android.hardware.nfc@1.0
+    $(PRIVATE_HIDL) -o $(PRIVATE_OUTPUT_DIR) \
+    -Lc++ -randroid.hardware:hardware/interfaces\
+    android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
 
-$(GEN): $(LOCAL_PATH)/types.hal $(LOCAL_PATH)/INfc.hal $(LOCAL_PATH)/INfcClientCallback.hal
+$(GEN): $(LOCAL_PATH)/types.hal
 	$(transform-generated-source)
+LOCAL_GENERATED_SOURCES += $(GEN)
 
+#
+# Build INfc.hal
+#
+GEN := $(intermediates)/android/hardware/nfc/1.0/NfcAll.cpp
+$(GEN): $(HIDL)
+$(GEN): PRIVATE_HIDL := $(HIDL)
+$(GEN): PRIVATE_DEPS := $(LOCAL_PATH)/INfc.hal
+$(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
+$(GEN): PRIVATE_CUSTOM_TOOL = \
+    $(PRIVATE_HIDL) -o $(PRIVATE_OUTPUT_DIR) \
+    -Lc++ -randroid.hardware:hardware/interfaces\
+    android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
+
+$(GEN): $(LOCAL_PATH)/INfc.hal
+	$(transform-generated-source)
+LOCAL_GENERATED_SOURCES += $(GEN)
+
+#
+# Build INfcClientCallback.hal
+#
+GEN := $(intermediates)/android/hardware/nfc/1.0/NfcClientCallbackAll.cpp
+$(GEN): $(HIDL)
+$(GEN): PRIVATE_HIDL := $(HIDL)
+$(GEN): PRIVATE_DEPS := $(LOCAL_PATH)/INfcClientCallback.hal
+$(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
+$(GEN): PRIVATE_CUSTOM_TOOL = \
+    $(PRIVATE_HIDL) -o $(PRIVATE_OUTPUT_DIR) \
+    -Lc++ -randroid.hardware:hardware/interfaces\
+    android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
+
+$(GEN): $(LOCAL_PATH)/INfcClientCallback.hal
+	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES += $(GEN)
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
-
 LOCAL_SHARED_LIBRARIES := \
   libhwbinder \
   libutils \
