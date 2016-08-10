7fd352e7b3f07600e31072b690978b9f5c689d4c
Iliyan Malchev <malchev@google.com>
2016-08-10 08:02:31 -0700

    script to auto-generate makefiles for HALs
    Change-Id: I9d21a811d8e2fc604702388ad1910476df16f43a
    Signed-off-by: Iliyan Malchev <malchev@google.com>

---
+++ update-makefiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d hardware/interfaces ] ; then
+  echo "Where is hardware/interfaces?";
+  exit 1;
+fi
+
+packages=$(pushd hardware/interfaces > /dev/null; \
+           find -type f -name Android.mk -exec dirname {} \; | \
+           cut -c3- | \
+           awk -F'/' \
+                '{printf("android.hardware"); for(i=1;i<NF;i++){printf(".%s", $i);}; printf("@%s\n", $NF);}'; \
+           popd > /dev/null)
+
+for p in $packages; do
+  echo "Updating $p";
+  hidl-gen -Lmakefile -r android.hardware:hardware/interfaces $p;
+done
