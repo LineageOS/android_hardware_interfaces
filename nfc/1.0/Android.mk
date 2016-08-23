6b4d6396269875e7e189c140a71a0da5a14219dd
Keun Soo Yim <yim@google.com>
2016-08-22 17:25:48 -0700

    regenerate Android.mk files
    Change-Id: I6a8b48943df469c9f345c05fb00c671f6f8dd393

--- nfc/1.0/Android.mk
+++ nfc/1.0/Android.mk
@@ -9,23 +9,6 @@ intermediates := $(local-generated-sources-dir)
 HIDL := $(HOST_OUT_EXECUTABLES)/hidl-gen$(HOST_EXECUTABLE_SUFFIX)
 
 #
-# Build types.hal
-#
-GEN := $(intermediates)/android/hardware/nfc/1.0/types.cpp
-$(GEN): $(HIDL)
-$(GEN): PRIVATE_HIDL := $(HIDL)
-$(GEN): PRIVATE_DEPS := $(LOCAL_PATH)/types.hal
-$(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
-$(GEN): PRIVATE_CUSTOM_TOOL = \
-    $(PRIVATE_HIDL) -o $(PRIVATE_OUTPUT_DIR) \
-    -Lc++ -randroid.hardware:hardware/interfaces\
-    android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
-
-$(GEN): $(LOCAL_PATH)/types.hal
-	$(transform-generated-source)
-LOCAL_GENERATED_SOURCES += $(GEN)
-
-#
 # Build INfc.hal
 #
 GEN := $(intermediates)/android/hardware/nfc/1.0/NfcAll.cpp
@@ -39,6 +22,23 @@ $(GEN): PRIVATE_CUSTOM_TOOL = \
     android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
 
 $(GEN): $(LOCAL_PATH)/INfc.hal
+	$(transform-generated-source)
+LOCAL_GENERATED_SOURCES += $(GEN)
+
+#
+# Build types.hal
+#
+GEN := $(intermediates)/android/hardware/nfc/1.0/types.cpp
+$(GEN): $(HIDL)
+$(GEN): PRIVATE_HIDL := $(HIDL)
+$(GEN): PRIVATE_DEPS := $(LOCAL_PATH)/types.hal
+$(GEN): PRIVATE_OUTPUT_DIR := $(intermediates)
+$(GEN): PRIVATE_CUSTOM_TOOL = \
+    $(PRIVATE_HIDL) -o $(PRIVATE_OUTPUT_DIR) \
+    -Lc++ -randroid.hardware:hardware/interfaces\
+    android.hardware.nfc@1.0::$(patsubst %.hal,%,$(notdir $(PRIVATE_DEPS)))
+
+$(GEN): $(LOCAL_PATH)/types.hal
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES += $(GEN)
 
