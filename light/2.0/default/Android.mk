41db6ab899b381599359b9b1dfe8f8e573b340d1
Steven Moreland <smoreland@google.com>
2016-10-03 09:20:36 -0700

    Adding android.hardware.light@2.0 service.
    Bug: 32022100
    Test: end-to-end
    Change-Id: I2d143f3a283e3a230c5ae4999e492f29ffe4234c

--- light/2.0/default/Android.mk
+++ light/2.0/default/Android.mk
@@ -18,3 +18,26 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.light@2.0 \
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := android.hardware.light@2.0-service
+LOCAL_INIT_RC := android.hardware.light@2.0-service.rc
+LOCAL_SRC_FILES := \
+    service.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    libhardware_legacy \
+    libhardware \
+
+LOCAL_SHARED_LIBRARIES += \
+    libhwbinder \
+    libhidl \
+    android.hardware.light@2.0 \
+
+include $(BUILD_EXECUTABLE)
