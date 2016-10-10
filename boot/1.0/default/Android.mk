cd0d29d4afd1910b584fb374265548adaf3f46ef
Connor O'Brien <connoro@google.com>
2016-10-10 14:41:35 -0700

    Add android.hardware.boot_control@1.0 service.
    Bug: 31864052
    Change-Id: I982077d71a81c0bc8d081f0cc7e83180b97f2b87
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- boot/1.0/default/Android.mk
+++ boot/1.0/default/Android.mk
@@ -15,3 +15,20 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.boot@1.0 \
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := android.hardware.boot@1.0-service
+LOCAL_INIT_RC := android.hardware.boot@1.0-service.rc
+LOCAL_SRC_FILES := \
+    service.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libhwbinder \
+    libhardware \
+    libhidl \
+    libutils \
+    android.hardware.boot@1.0 \
+
+include $(BUILD_EXECUTABLE)
