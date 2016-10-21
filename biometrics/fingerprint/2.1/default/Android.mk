965bd32a70f17836796124dd7e3dc8c44a1449c5
Sasha Levitskiy <sanek@google.com>
2016-10-21 10:55:25 -0700

    Fingerprint: HIDL: add default HAL 2.1 implementation
    Test: Manual fingerprint functionality verification.
    
    Bug: 31973669
    Change-Id: Ib8bcf602ba0c8884b13d31eb23baa88a912ace20
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

---
+++ biometrics/fingerprint/2.1/default/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-impl
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    BiometricsFingerprint.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libhidl \
+    libhardware \
+    libhwbinder \
+    libutils \
+    android.hardware.biometrics.fingerprint@2.1 \
+
+include $(BUILD_SHARED_LIBRARY)
