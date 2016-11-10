3e1d94cfe781505b6709538665cb66cc1e10ed07
Sandeep Patil <sspatil@google.com>
2016-11-10 09:22:27 -0800

    health: add default health HAL service implementation
    Test: Tested with stub HAL on angler
    Bug: b/32754732
    
    Change-Id: I54a5ac536e1f34cf2498972653aef702cd926b2a
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- health/1.0/default/Android.mk
+++ health/1.0/default/Android.mk
@@ -34,4 +34,23 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := android.hardware.health@1.0-service
+LOCAL_INIT_RC := android.hardware.health@1.0-service.rc
+LOCAL_SRC_FILES := \
+    HealthService.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    libhwbinder \
+    libhidl \
+    android.hardware.health@1.0 \
+
+include $(BUILD_EXECUTABLE)
+
 include $(call first-makefiles-under,$(LOCAL_PATH))
