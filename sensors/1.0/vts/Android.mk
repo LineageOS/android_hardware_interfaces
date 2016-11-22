bba9d9a18f796e87b6f0fcfe315a5e61b179ece5
Peng Xu <pengxu@google.com>
2016-11-21 20:14:41 -0800

    Added sensor target VTS test
    Initial submission of Sensor VTS test, including the following
    test cases:
    
     * SensorListValid
     * NormalAccelerometerStreamingOperation
     * NormalGyroscopeStreamingOperation
     * AccelerometerSamplingPeriodHotSwitchOperation
     * AccelerometerBatchingOperation
    
    One thing to note is that runtime has to be stopped to prevent
    sensor service from stealing samples (which causes test failure).
    
    Test: m vts -j32 &&
          adb shell stop &&
          vts-tradefed run commandAndExit vts \
              --skip-all-system-status-check \
              --primary-abi-only \
              --skip-preconditions \
              --module SensorsHidlTargetTest
          vts-tradefed run commandAndExit vts \
              --skip-all-system-status-check \
              --primary-abi-only \
              --skip-preconditions \
              --module SensorsHidlTest
          adb shell start
    
    Test: # alternative, faster way to run target test only
          m sensors_hidl_hal_test -j32  &&
          TOUT=$T/out/target/product/${TARGET_PRODUCT}
          TEST=data/nativetest/sensors_hidl_hal_test/sensors_hidl_hal_test
          adb push ${TOUT}/${TEST} /${TEST} && \
          adb shell ${TEST}
    
    Change-Id: I3e26aa550f4cdec2cebb847f47d63ed33a527210

---
+++ sensors/1.0/vts/Android.mk
@@ -0,0 +1,83 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# build VTS driver for Sensors v1.0.
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libvts_driver_hidl_sensors@1.0
+
+LOCAL_SRC_FILES := \
+  Sensors.vts \
+  types.vts \
+
+LOCAL_SHARED_LIBRARIES += \
+  android.hardware.sensors@1.0 \
+  libbase \
+  libutils \
+  libcutils \
+  liblog \
+  libhidlbase \
+  libhidltransport \
+  libhwbinder \
+  libprotobuf-cpp-full \
+  libvts_common \
+  libvts_datatype \
+  libvts_measurement \
+  libvts_multidevice_proto \
+
+LOCAL_PROTOC_OPTIMIZE_TYPE := full
+
+LOCAL_MULTILIB := both
+
+include $(BUILD_SHARED_LIBRARY)
+
+# build profiler for sensors.
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libvts_profiler_hidl_sensors@1.0
+
+LOCAL_SRC_FILES := \
+  Sensors.vts \
+  types.vts \
+
+LOCAL_C_INCLUDES += \
+  test/vts/drivers/libprofiling \
+
+LOCAL_VTS_MODE := PROFILER
+
+LOCAL_SHARED_LIBRARIES := \
+   android.hardware.sensors@1.0 \
+   libbase \
+   libutils \
+   libcutils \
+   liblog \
+   libhidlbase \
+   libhidltransport \
+   libhwbinder \
+   libprotobuf-cpp-full \
+   libvts_common \
+   libvts_multidevice_proto \
+   libvts_profiling \
+
+LOCAL_PROTOC_OPTIMIZE_TYPE := full
+
+include $(BUILD_SHARED_LIBRARY)
+
+# include hidl test makefiles
+include $(LOCAL_PATH)/functional/vts/testcases/hal/sensors/hidl/Android.mk
+
