335015939hi/dumbos-platform_hardware_interfaces

Add android.hardware.consumerir@1.0 service.
Test: make android.hardware.consumerir@1.0-service
Change-Id: Ife45f98836d4d5a3b40d26f21d43e1903fdb8fb3
Signed-off-by: Connor O'Brien <connoro@google.com>

consumerir/1.0/default/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.consumerir@1.0-service"
+
+#include <android/hardware/consumerir/1.0/IConsumerir.h>
+#include <hidl/LegacySupport.h>
+
+using android::hardware::consumerir::V1_0::IConsumerir;
+using android::hardware::defaultPassthroughServiceImplementation;
+
+int main() {
+    return defaultPassthroughServiceImplementation<IConsumerir>("consumerir");
+}

LineageOS/android_hardware_interfaces

ir/1.0/default/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.ir@1.0-service"
+
+#include <android/hardware/ir/1.0/IConsumerIr.h>
+#include <hidl/LegacySupport.h>
+
+using android::hardware::ir::V1_0::IConsumerIr;
+using android::hardware::defaultPassthroughServiceImplementation;
+
+int main() {
+    return defaultPassthroughServiceImplementation<IConsumerIr>("consumerir");
+}