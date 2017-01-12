ff4224809f8cf3e6d56c1dfdfd21adf340390e99
Steven Moreland <smoreland@google.com>
2017-01-12 13:33:16 -0800

    Move hidl shims to the vendor partition.
    We need google shims on the vendor partition because they are providing
    an implementation of a vendor defined interface. They were written by
    google just as a courtesy/to make the transition easier. They're
    basically a set for vendors to assemble their hal implementations
    from.
    
    Bug: 34135607
    Test: marlin persist.hal.binderization on/off
    Change-Id: I3f86c77c147514fe470f08088a66de312ae32d4e

--- boot/1.0/default/Android.mk
+++ boot/1.0/default/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.boot@1.0-impl
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := \
     BootControl.cpp \
 
@@ -19,6 +20,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := android.hardware.boot@1.0-service
 LOCAL_INIT_RC := android.hardware.boot@1.0-service.rc
 LOCAL_SRC_FILES := \
