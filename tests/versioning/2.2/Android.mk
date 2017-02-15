821b01c9fe2fc732ea0c79da9501564ea815cd54
Steven Moreland <smoreland@google.com>
2017-02-15 10:59:23 -0800

    update-makefiles.sh
    There have been some changes to the way hidl-gen generates these and
    some CLs got out of sync with hidl-gen.
    
    Test: pass
    Change-Id: I33c573499d5dd37de447902fd93929b80a5df371

--- tests/versioning/2.2/Android.mk
+++ tests/versioning/2.2/Android.mk
@@ -8,7 +8,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.tests.versioning@2.2-java
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
-intermediates := $(local-generated-sources-dir)
+intermediates := $(call local-generated-sources-dir, COMMON)
 
 HIDL := $(HOST_OUT_EXECUTABLES)/hidl-gen$(HOST_EXECUTABLE_SUFFIX)
 
@@ -62,7 +62,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.tests.versioning@2.2-java-static
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
-intermediates := $(local-generated-sources-dir)
+intermediates := $(call local-generated-sources-dir, COMMON)
 
 HIDL := $(HOST_OUT_EXECUTABLES)/hidl-gen$(HOST_EXECUTABLE_SUFFIX)
 
