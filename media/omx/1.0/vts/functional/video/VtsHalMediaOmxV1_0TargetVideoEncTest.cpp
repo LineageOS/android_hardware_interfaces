a558d9d5fd49ed7b730162c92ef829b85becca91
Ram Mohan M <ram.mohan@ittiam.com>
2017-04-28 12:18:35 +0530

    Omx vts tests.
    Bug: 32023356
    Test: make vts.
    
    cover basic encoder & decoder test to all components
    
    Till the previous commit, few components went untested.
    This commit covers all softomx components.
    
    Test: tested on softomx components
    
    Change-Id: I4c996a3743a6190d9c6d4c835aa89daefdc14658
    
    enumerate profile and level for encoder components
    
    Change-Id: I4b9d3b3106d33a0241a7e36bd7189c1b3ef40ef0
    
    bug fix: omx android compile as 32bit on 64bit platforms
    
    thanks lajos
    
    Change-Id: If11b20eeb6a75319f9e0cd6a62524dadab2ba8af
    
    Add more VTS tests for IOmxStore and IOmxNode
    
    Change-Id: I3401025b4f0b821f469222707c75b49fe4f0b0c5
    
    avoid false flagging of errors
    
    If testing of the given component is not supported application do not flag
    an error, instead exit gracefully.
    
    Change-Id: Ic91a92614d18b204cc50b7d8f6dc1a0ca81ff782
    
    Add support for g711, gsm and raw audio decoder components
    
    Change-Id: Ied70abdabb397a7a63e13293a8b35365a6b23e4f
    
    split eos test
    
    the eos test in decoders handles three different cases. These are now
    separated in to different test fixtures.
    
    Change-Id: I30c102109ce60a345f878d03d481337462fc817f
    
    Change-Id: I3401025b4f0b821f469222707c75b49fe4f0b0c5

--- media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoEncTest.cpp
+++ media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoEncTest.cpp
@@ -15,6 +15,10 @@
  */
 
 #define LOG_TAG "media_omx_hidl_video_enc_test"
+#ifdef __LP64__
+#define OMX_ANDROID_COMPILE_AS_32BIT_ON_64BIT_PLATFORMS
+#endif
+
 #include <android-base/logging.h>
 
 #include <android/hardware/media/omx/1.0/IOmx.h>
@@ -41,6 +45,7 @@ using ::android::sp;
 
 #include <VtsHalHidlTargetTestBase.h>
 #include <getopt.h>
+#include <media/hardware/HardwareAPI.h>
 #include <media_hidl_test_common.h>
 #include <media_video_hidl_test_common.h>
 #include <fstream>
@@ -130,14 +135,15 @@ static ComponentTestEnvironment* gEnv = nullptr;
 class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
    public:
     virtual void SetUp() override {
+        disableTest = false;
         android::hardware::media::omx::V1_0::Status status;
         omx = ::testing::VtsHalHidlTargetTestBase::getService<IOmx>(
             gEnv->getInstance());
         ASSERT_NE(omx, nullptr);
         observer = new CodecObserver([this](Message msg) { (void)msg; });
         ASSERT_NE(observer, nullptr);
-        ASSERT_EQ(strncmp(gEnv->getComponent().c_str(), "OMX.", 4), 0)
-            << "Invalid Component Name";
+        if (strncmp(gEnv->getComponent().c_str(), "OMX.", 4) != 0)
+            disableTest = true;
         EXPECT_TRUE(omx->allocateNode(
                            gEnv->getComponent(), observer,
                            [&](android::hardware::media::omx::V1_0::Status _s,
@@ -153,7 +159,7 @@ class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
             standardComp CompName;
         };
         const StringToName kStringToName[] = {
-            {"h263", h263}, {"avc", avc}, {"mpeg2", mpeg2}, {"mpeg4", mpeg4},
+            {"h263", h263}, {"avc", avc}, {"mpeg4", mpeg4},
             {"hevc", hevc}, {"vp8", vp8}, {"vp9", vp9},
         };
         const size_t kNumStringToName =
@@ -170,16 +176,15 @@ class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
                 break;
             }
         }
-        ASSERT_NE(compName, unknown_comp);
+        if (compName == unknown_comp) disableTest = true;
         struct CompToCompression {
             standardComp CompName;
             OMX_VIDEO_CODINGTYPE eCompressionFormat;
         };
         static const CompToCompression kCompToCompression[] = {
             {h263, OMX_VIDEO_CodingH263},   {avc, OMX_VIDEO_CodingAVC},
-            {mpeg2, OMX_VIDEO_CodingMPEG2}, {mpeg4, OMX_VIDEO_CodingMPEG4},
-            {hevc, OMX_VIDEO_CodingHEVC},   {vp8, OMX_VIDEO_CodingVP8},
-            {vp9, OMX_VIDEO_CodingVP9},
+            {mpeg4, OMX_VIDEO_CodingMPEG4}, {hevc, OMX_VIDEO_CodingHEVC},
+            {vp8, OMX_VIDEO_CodingVP8},     {vp9, OMX_VIDEO_CodingVP9},
         };
         static const size_t kNumCompToCompression =
             sizeof(kCompToCompression) / sizeof(kCompToCompression[0]);
@@ -190,7 +195,8 @@ class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
                 break;
             }
         }
-        ASSERT_NE(i, kNumCompToCompression);
+        if (i == kNumCompToCompression) disableTest = true;
+        if (disableTest) std::cerr << "[          ] Warning !  Test Disabled\n";
     }
 
     virtual void TearDown() override {
@@ -203,7 +209,6 @@ class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
     enum standardComp {
         h263,
         avc,
-        mpeg2,
         mpeg4,
         hevc,
         vp8,
@@ -216,12 +221,87 @@ class VideoEncHidlTest : public ::testing::VtsHalHidlTargetTestBase {
     sp<IOmxNode> omxNode;
     standardComp compName;
     OMX_VIDEO_CODINGTYPE eCompressionFormat;
+    bool disableTest;
 
    protected:
     static void description(const std::string& description) {
         RecordProperty("description", description);
     }
 };
+
+// request VOP refresh
+void requestIDR(sp<IOmxNode> omxNode, OMX_U32 portIndex) {
+    android::hardware::media::omx::V1_0::Status status;
+    OMX_CONFIG_INTRAREFRESHVOPTYPE param;
+    param.IntraRefreshVOP = OMX_TRUE;
+    status = setPortConfig(omxNode, OMX_IndexConfigVideoIntraVOPRefresh,
+                           portIndex, &param);
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr << "[          ] Warning ! unable to request IDR \n";
+}
+
+// modify bitrate
+void changeBitrate(sp<IOmxNode> omxNode, OMX_U32 portIndex, uint32_t nBitrate) {
+    android::hardware::media::omx::V1_0::Status status;
+    OMX_VIDEO_CONFIG_BITRATETYPE param;
+    param.nEncodeBitrate = nBitrate;
+    status =
+        setPortConfig(omxNode, OMX_IndexConfigVideoBitrate, portIndex, &param);
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr << "[          ] Warning ! unable to change Bitrate \n";
+}
+
+// modify framerate
+Return<android::hardware::media::omx::V1_0::Status> changeFrameRate(
+    sp<IOmxNode> omxNode, OMX_U32 portIndex, uint32_t xFramerate) {
+    android::hardware::media::omx::V1_0::Status status;
+    OMX_CONFIG_FRAMERATETYPE param;
+    param.xEncodeFramerate = xFramerate;
+    status = setPortConfig(omxNode, OMX_IndexConfigVideoFramerate, portIndex,
+                           &param);
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr << "[          ] Warning ! unable to change Framerate \n";
+    return status;
+}
+
+// modify intra refresh interval
+void changeRefreshPeriod(sp<IOmxNode> omxNode, OMX_U32 portIndex,
+                         uint32_t nRefreshPeriod) {
+    android::hardware::media::omx::V1_0::Status status;
+    OMX_VIDEO_CONFIG_ANDROID_INTRAREFRESHTYPE param;
+    param.nRefreshPeriod = nRefreshPeriod;
+    status = setPortConfig(omxNode,
+                           (OMX_INDEXTYPE)OMX_IndexConfigAndroidIntraRefresh,
+                           portIndex, &param);
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr << "[          ] Warning ! unable to change Refresh Period\n";
+}
+
+// set intra refresh interval
+void setRefreshPeriod(sp<IOmxNode> omxNode, OMX_U32 portIndex,
+                      uint32_t nRefreshPeriod) {
+    android::hardware::media::omx::V1_0::Status status;
+    OMX_VIDEO_PARAM_INTRAREFRESHTYPE param;
+    param.eRefreshMode = OMX_VIDEO_IntraRefreshCyclic;
+    param.nCirMBs = 0;
+    if (nRefreshPeriod == 0)
+        param.nCirMBs = 0;
+    else {
+        OMX_PARAM_PORTDEFINITIONTYPE portDef;
+        status = getPortParam(omxNode, OMX_IndexParamPortDefinition, portIndex,
+                              &portDef);
+        if (status == ::android::hardware::media::omx::V1_0::Status::OK) {
+            param.nCirMBs =
+                ((portDef.format.video.nFrameWidth + 15) >>
+                 4 * (portDef.format.video.nFrameHeight + 15) >> 4) /
+                nRefreshPeriod;
+        }
+    }
+    status = setPortParam(omxNode, OMX_IndexParamVideoIntraRefresh, portIndex,
+                          &param);
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr << "[          ] Warning ! unable to set Refresh Period \n";
+}
 
 // Set Default port param.
 void setDefaultPortParam(sp<IOmxNode> omxNode, OMX_U32 portIndex,
@@ -241,10 +321,49 @@ void setDefaultPortParam(sp<IOmxNode> omxNode, OMX_U32 portIndex,
                           &portDef);
     EXPECT_EQ(status, ::android::hardware::media::omx::V1_0::Status::OK);
 
+    std::vector<int32_t> arrProfile;
+    std::vector<int32_t> arrLevel;
+    enumerateProfileAndLevel(omxNode, portIndex, &arrProfile, &arrLevel);
+    if (arrProfile.empty() == true || arrLevel.empty() == true)
+        ASSERT_TRUE(false);
+    int32_t profile = arrProfile[0];
+    int32_t level = arrLevel[0];
+
     switch ((int)eCompressionFormat) {
         case OMX_VIDEO_CodingAVC:
-            setupAVCPort(omxNode, portIndex, OMX_VIDEO_AVCProfileBaseline,
-                         OMX_VIDEO_AVCLevel31, xFramerate);
+            setupAVCPort(omxNode, portIndex,
+                         static_cast<OMX_VIDEO_AVCPROFILETYPE>(profile),
+                         static_cast<OMX_VIDEO_AVCLEVELTYPE>(level),
+                         xFramerate);
+            break;
+        case OMX_VIDEO_CodingHEVC:
+            setupHEVCPort(omxNode, portIndex,
+                          static_cast<OMX_VIDEO_HEVCPROFILETYPE>(profile),
+                          static_cast<OMX_VIDEO_HEVCLEVELTYPE>(level));
+            break;
+        case OMX_VIDEO_CodingH263:
+            setupH263Port(omxNode, portIndex,
+                          static_cast<OMX_VIDEO_H263PROFILETYPE>(profile),
+                          static_cast<OMX_VIDEO_H263LEVELTYPE>(level),
+                          xFramerate);
+            break;
+        case OMX_VIDEO_CodingMPEG4:
+            setupMPEG4Port(omxNode, portIndex,
+                           static_cast<OMX_VIDEO_MPEG4PROFILETYPE>(profile),
+                           static_cast<OMX_VIDEO_MPEG4LEVELTYPE>(level),
+                           xFramerate);
+            break;
+        case OMX_VIDEO_CodingVP8:
+            setupVPXPort(omxNode, portIndex, xFramerate);
+            setupVP8Port(omxNode, portIndex,
+                         static_cast<OMX_VIDEO_VP8PROFILETYPE>(profile),
+                         static_cast<OMX_VIDEO_VP8LEVELTYPE>(level));
+            break;
+        case OMX_VIDEO_CodingVP9:
+            setupVPXPort(omxNode, portIndex, xFramerate);
+            setupVP9Port(omxNode, portIndex,
+                         static_cast<OMX_VIDEO_VP9PROFILETYPE>(profile),
+                         static_cast<OMX_VIDEO_VP9LEVELTYPE>(level));
             break;
         default:
             break;
@@ -252,32 +371,27 @@ void setDefaultPortParam(sp<IOmxNode> omxNode, OMX_U32 portIndex,
 }
 
 // LookUpTable of clips and metadata for component testing
-void GetURLForComponent(VideoEncHidlTest::standardComp comp, char* mURL) {
-    struct CompToURL {
-        VideoEncHidlTest::standardComp comp;
-        const char* mURL;
-    };
-    static const CompToURL kCompToURL[] = {
-        {VideoEncHidlTest::standardComp::avc,
-         "bbb_480x360_420p_30fps_32frames.yuv"},
-    };
-
-    for (size_t i = 0; i < sizeof(kCompToURL) / sizeof(kCompToURL[0]); ++i) {
-        if (kCompToURL[i].comp == comp) {
-            strcat(mURL, kCompToURL[i].mURL);
-            return;
-        }
-    }
+void GetURLForComponent(char* URL) {
+    strcat(URL, "bbb_352x288_420p_30fps_32frames.yuv");
 }
 
 // Encode N Frames
 void encodeNFrames(sp<IOmxNode> omxNode, sp<CodecObserver> observer,
+                   OMX_U32 portIndexOutput,
                    android::Vector<BufferInfo>* iBuffer,
                    android::Vector<BufferInfo>* oBuffer, uint32_t nFrames,
                    uint32_t xFramerate, int bytesCount,
                    std::ifstream& eleStream) {
     android::hardware::media::omx::V1_0::Status status;
     Message msg;
+    uint32_t ipCount = 0;
+
+    if (ipCount == 0) {
+        status = changeFrameRate(omxNode, portIndexOutput, (24U << 16));
+        if (status == ::android::hardware::media::omx::V1_0::Status::OK)
+            xFramerate = (24U << 16);
+    }
+
     // dispatch output buffers
     for (size_t i = 0; i < oBuffer->size(); i++) {
         dispatchOutputBuffer(omxNode, oBuffer, i);
@@ -295,14 +409,23 @@ void encodeNFrames(sp<IOmxNode> omxNode, sp<CodecObserver> observer,
         dispatchInputBuffer(omxNode, iBuffer, i, bytesCount, 0, timestamp);
         timestamp += timestampIncr;
         nFrames--;
+        ipCount++;
     }
 
     while (1) {
         status =
             observer->dequeueMessage(&msg, DEFAULT_TIMEOUT, iBuffer, oBuffer);
 
-        if (status == android::hardware::media::omx::V1_0::Status::OK)
-            ASSERT_TRUE(false);
+        if (status == android::hardware::media::omx::V1_0::Status::OK) {
+            ASSERT_EQ(msg.type, Message::Type::EVENT);
+            if (msg.data.eventData.event == OMX_EventPortSettingsChanged) {
+                ASSERT_EQ(msg.data.eventData.data1, portIndexOutput);
+                ASSERT_EQ(msg.data.eventData.data2,
+                          OMX_IndexConfigAndroidIntraRefresh);
+            } else {
+                ASSERT_TRUE(false);
+            }
+        }
 
         if (nFrames == 0) break;
 
@@ -319,9 +442,15 @@ void encodeNFrames(sp<IOmxNode> omxNode, sp<CodecObserver> observer,
                                 timestamp);
             timestamp += timestampIncr;
             nFrames--;
+            ipCount++;
         }
         if ((index = getEmptyBufferID(oBuffer)) < oBuffer->size()) {
             dispatchOutputBuffer(omxNode, oBuffer, index);
+        }
+        if (ipCount == 15) {
+            changeBitrate(omxNode, portIndexOutput, 768000);
+            requestIDR(omxNode, portIndexOutput);
+            changeRefreshPeriod(omxNode, portIndexOutput, 15);
         }
     }
 }
@@ -329,6 +458,7 @@ void encodeNFrames(sp<IOmxNode> omxNode, sp<CodecObserver> observer,
 // set component role
 TEST_F(VideoEncHidlTest, SetRole) {
     description("Test Set Component Role");
+    if (disableTest) return;
     android::hardware::media::omx::V1_0::Status status;
     status = setRole(omxNode, gEnv->getRole().c_str());
     ASSERT_EQ(status, ::android::hardware::media::omx::V1_0::Status::OK);
@@ -337,6 +467,7 @@ TEST_F(VideoEncHidlTest, SetRole) {
 // port format enumeration
 TEST_F(VideoEncHidlTest, EnumeratePortFormat) {
     description("Test Component on Mandatory Port Parameters (Port Format)");
+    if (disableTest) return;
     android::hardware::media::omx::V1_0::Status status;
     uint32_t kPortIndexInput = 0, kPortIndexOutput = 1;
     OMX_COLOR_FORMATTYPE eColorFormat = OMX_COLOR_FormatYUV420Planar;
@@ -363,6 +494,7 @@ TEST_F(VideoEncHidlTest, EnumeratePortFormat) {
 // test raw stream encode
 TEST_F(VideoEncHidlTest, EncodeTest) {
     description("Test Encode");
+    if (disableTest) return;
     android::hardware::media::omx::V1_0::Status status;
     uint32_t kPortIndexInput = 0, kPortIndexOutput = 1;
     status = setRole(omxNode, gEnv->getRole().c_str());
@@ -376,23 +508,40 @@ TEST_F(VideoEncHidlTest, EncodeTest) {
     }
     char mURL[512];
     strcpy(mURL, gEnv->getRes().c_str());
-    GetURLForComponent(compName, mURL);
+    GetURLForComponent(mURL);
 
     std::ifstream eleStream;
     eleStream.open(mURL, std::ifstream::binary);
     ASSERT_EQ(eleStream.is_open(), true);
 
     // Configure input port
-    uint32_t nFrameWidth = 480;
-    uint32_t nFrameHeight = 360;
+    uint32_t nFrameWidth = 352;
+    uint32_t nFrameHeight = 288;
     uint32_t xFramerate = (30U << 16);
     OMX_COLOR_FORMATTYPE eColorFormat = OMX_COLOR_FormatYUV420Planar;
     setupRAWPort(omxNode, kPortIndexInput, nFrameWidth, nFrameHeight, 0,
                  xFramerate, eColorFormat);
     // Configure output port
-    uint32_t nBitRate = 768000;
+    uint32_t nBitRate = 512000;
     setDefaultPortParam(omxNode, kPortIndexOutput, eCompressionFormat, nBitRate,
                         xFramerate);
+    setRefreshPeriod(omxNode, kPortIndexOutput, 0);
+    unsigned int index;
+    omxNode->getExtensionIndex(
+        "OMX.google.android.index.prependSPSPPSToIDRFrames",
+        [&status, &index](android::hardware::media::omx::V1_0::Status _s,
+                          unsigned int _nl) {
+            status = _s;
+            index = _nl;
+        });
+    if (status == ::android::hardware::media::omx::V1_0::Status::OK) {
+        android::PrependSPSPPSToIDRFramesParams param;
+        param.bEnable = OMX_TRUE;
+        status = setParam(omxNode, static_cast<OMX_INDEXTYPE>(index), &param);
+    }
+    if (status != ::android::hardware::media::omx::V1_0::Status::OK)
+        std::cerr
+            << "[          ] Warning ! unable to prependSPSPPSToIDRFrames\n";
 
     android::Vector<BufferInfo> iBuffer, oBuffer;
 
@@ -402,8 +551,8 @@ TEST_F(VideoEncHidlTest, EncodeTest) {
     // set state to executing
     changeStateIdletoExecute(omxNode, observer);
 
-    encodeNFrames(omxNode, observer, &iBuffer, &oBuffer, 1024, xFramerate,
-                  (nFrameWidth * nFrameHeight * 3) >> 1, eleStream);
+    encodeNFrames(omxNode, observer, kPortIndexOutput, &iBuffer, &oBuffer, 1024,
+                  xFramerate, (nFrameWidth * nFrameHeight * 3) >> 1, eleStream);
     // set state to idle
     changeStateExecutetoIdle(omxNode, observer, &iBuffer, &oBuffer);
     // set state to executing
