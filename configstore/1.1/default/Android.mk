812e85e126ccf1d9d17aa7342dbb1ccf9404cc15
Jaesoo Lee <jaesoo@google.com>
2017-04-28 18:29:40 +0900

    configstore: configstore HAL is up-revisioned to 1.1
    This change provides a reference implementation of the configstore HAL
    v1.1.
    
    Bug: 37727469
    Test: Built sailfish-userdebug and configstore-1.1 works
    Change-Id: I75e7fd1da8e90ae48d779a3ba28957c5a93a5529

--- configstore/1.1/default/Android.mk
+++ configstore/1.1/default/Android.mk
@@ -2,17 +2,18 @@ LOCAL_PATH := $(call my-dir)
 
 ################################################################################
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.configstore@1.0-service
+LOCAL_MODULE := android.hardware.configstore@1.1-service
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_INIT_RC := android.hardware.configstore@1.0-service.rc
+LOCAL_INIT_RC := android.hardware.configstore@1.1-service.rc
 LOCAL_SRC_FILES:= service.cpp
 
 include $(LOCAL_PATH)/surfaceflinger.mk
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.configstore@1.0 \
+    android.hardware.configstore@1.1 \
     libhidlbase \
     libhidltransport \
     libbase \
