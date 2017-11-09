85f8d2ec19b953818e7e2a47f2fc0d683d7456c6
Dan Willemsen <dwillemsen@google.com>
2017-11-08 16:05:10 -0800

    Replace OUT with OUT_DIR
    OUT is only defined if some functions in envsetup.sh are run, which is
    not the case on the build servers. I'm looking at removing the
    environment variable in local builds to keep things consistent.
    
    In this case, OUT_DIR was actually expected, not OUT, which is
    equivalent to PRODUCT_OUT.
    
    Test: none
    Change-Id: I1e5e9f40727104716212d696927d1a32d7a74fab

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -52,7 +52,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/bin/hw/android.hardware.blue
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/hw/android.hardware.bluetooth*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/hw/android.hardware.bluetooth*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/init/android.hardware.bluetooth*)
-$(call add-clean-step, rm -rf $(OUT)/soong/.intermediates/)
+$(call add-clean-step, rm -rf $(OUT_DIR)/soong/.intermediates/)
 $(call add-clean-step, rm -rf $(OUT_DIR)/soong/.intermediates/hardware/interfaces/)
 $(call add-clean-step, rm -rf $(OUT_DIR)/soong/.intermediates/hardware/interfaces/)
 $(call add-clean-step, find $(PRODUCT_OUT)/system $(PRODUCT_OUT)/vendor -type f -name "android\.hardware\.configstore*" -print0 | xargs -0 rm -f)
