d31932aa672679dc775b88a3c30a474156dcacbb
Hridya Valsaraju <hridya@google.com>
2018-01-17 23:09:24 -0800

    Modify IHealthInfoCallback interface to return V2.0 HealthInfo
    Bug: 71860528
    Test: vts-tradefed run vts -m VtsHalHealthV2_0
    Change-Id: Ie7294efa644442825baa5f08c39553c10b368e75

--- health/2.0/default/include/health2/Health.h
+++ health/2.0/default/include/health2/Health.h
@@ -22,7 +22,6 @@ namespace V2_0 {
 namespace implementation {
 
 using V1_0::BatteryStatus;
-using V1_0::HealthInfo;
 
 using ::android::hidl::base::V1_0::IBase;
 
@@ -38,7 +37,7 @@ struct Health : public IHealth, hidl_death_recipient {
     Health(struct healthd_config* c);
 
     // TODO(b/62229583): clean up and hide these functions after update() logic is simplified.
-    void notifyListeners(const HealthInfo& info);
+    void notifyListeners(HealthInfo* info);
 
     // Methods from IHealth follow.
     Return<Result> registerCallback(const sp<IHealthInfoCallback>& callback) override;
