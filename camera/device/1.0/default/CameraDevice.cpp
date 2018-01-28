9b5689824d85bc44a96af790633e9bd77cd2c102
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-28 11:34:01 -0800

    camera: Only link and use vendor.qti.hardware.camera.device if specified
    Only use if TARGET_USES_QTI_CAMERA_DEVICE is set
    
    Change-Id: I74b178c2f685a2ffda3e91eb7ff6d05664eebaf6

--- camera/device/1.0/default/CameraDevice.cpp
+++ camera/device/1.0/default/CameraDevice.cpp
@@ -428,6 +428,7 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
              index, mem->mNumBufs);
         return;
     }
+#ifdef QTI_CAMERA_DEVICE
     if(object->mQDeviceCallback != nullptr) {
          vendor::qti::hardware::camera::device::V1_0::QCameraFrameMetadata hidlMetadata;
          if (metadata) {
@@ -465,6 +466,7 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
          object->mQDeviceCallback->QDataCallback(
                  (DataCallbackMsg) msg_type, mem->handle.mId, index, hidlMetadata);
     } else {
+#endif
        if (object->mDeviceCallback != nullptr) {
            CameraFrameMetadata hidlMetadata;
            if (metadata) {
@@ -489,7 +491,9 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
            CameraHeapMemory* mem = static_cast<CameraHeapMemory *>(data->handle);
            object->mDeviceCallback->dataCallback(
                    (DataCallbackMsg) msg_type, mem->handle.mId, index, hidlMetadata);
+#ifdef QTI_CAMERA_DEVICE
        }
+#endif
     }
 }
 
@@ -705,11 +709,13 @@ Return<Status> CameraDevice::open(const sp<ICameraDeviceCallback>& callback) {
 
     initHalPreviewWindow();
     mDeviceCallback = callback;
+#ifdef QTI_CAMERA_DEVICE
     mQDeviceCallback =
         vendor::qti::hardware::camera::device::V1_0::IQCameraDeviceCallback::castFrom(callback);
     if(mQDeviceCallback == nullptr) {
         ALOGI("could not cast ICameraDeviceCallback to IQCameraDeviceCallback");
     }
+#endif
 
     if (mDevice->ops->set_callbacks) {
         mDevice->ops->set_callbacks(mDevice,
