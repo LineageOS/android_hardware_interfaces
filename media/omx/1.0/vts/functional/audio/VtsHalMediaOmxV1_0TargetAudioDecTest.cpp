b92f1d2e1be35b331b18589a8cde81d51ddd31d6
Pawin Vongmasa <pawin@google.com>
2018-05-10 19:05:09 -0700

    Disable tests for secure audio decoders
    Test: make vts -j123 && vts-tradefed run commandAndExit vts \
    --skip-all-system-status-check \
    --skip-preconditions -m VtsHalMediaOmxV1_0Host \
    -l INFO
    
    Bug: 70933963
    Change-Id: Iaaf3b7d382e7df4374dd7e03e5cc7b2ae6861fad

--- media/omx/1.0/vts/functional/audio/VtsHalMediaOmxV1_0TargetAudioDecTest.cpp
+++ media/omx/1.0/vts/functional/audio/VtsHalMediaOmxV1_0TargetAudioDecTest.cpp
@@ -209,7 +209,16 @@ class AudioDecHidlTest : public ::testing::VtsHalHidlTargetTestBase {
         framesReceived = 0;
         timestampUs = 0;
         timestampDevTest = false;
-        if (disableTest) std::cerr << "[          ] Warning !  Test Disabled\n";
+        isSecure = false;
+        size_t suffixLen = strlen(".secure");
+        if (strlen(gEnv->getComponent().c_str()) >= suffixLen) {
+            isSecure =
+                !strcmp(gEnv->getComponent().c_str() +
+                            strlen(gEnv->getComponent().c_str()) - suffixLen,
+                        ".secure");
+        }
+        if (isSecure) disableTest = true;
+        if (disableTest) std::cout << "[   WARN   ] Test Disabled \n";
     }
 
     virtual void TearDown() override {
@@ -300,6 +309,7 @@ class AudioDecHidlTest : public ::testing::VtsHalHidlTargetTestBase {
     OMX_AUDIO_CODINGTYPE eEncoding;
     bool disableTest;
     bool eosFlag;
+    bool isSecure;
     uint32_t framesReceived;
     uint64_t timestampUs;
     ::android::List<uint64_t> timestampUslist;
