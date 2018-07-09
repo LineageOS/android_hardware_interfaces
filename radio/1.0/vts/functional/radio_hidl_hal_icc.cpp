f3791f6a3fdc5ecee223772187eea3d9c093c4f0
sqian <shuoq@google.com>
2018-07-09 14:45:12 -0700

    Add SIM_PUK2 for Icc VTS tests
    Add SIM_PUK2 for supplyIccPin2ForApp and changeIccPin2ForApp if sim
    card is in the puk2 state.
    
    Bug: 111211929
    Test: sanity
    Change-Id: I80d924cc4a61565887cbd2a65ee5927a42ad656e
    Merged-In: I80d924cc4a61565887cbd2a65ee5927a42ad656e
    (cherry picked from commit 4ed0a216adc3bc9460d11d302ba53db9707ded44)

--- radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
@@ -90,7 +90,8 @@ TEST_F(RadioHidlTest, supplyIccPin2ForApp) {
             EXPECT_EQ(std::cv_status::no_timeout, wait());
             EXPECT_EQ(serial, radioRsp->rspInfo.serial);
             EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
-            EXPECT_EQ(RadioError::PASSWORD_INCORRECT, radioRsp->rspInfo.error);
+            ASSERT_TRUE(CheckAnyOfErrors(radioRsp->rspInfo.error,
+                                         {RadioError::PASSWORD_INCORRECT, RadioError::SIM_PUK2}));
         }
     }
 }
@@ -161,9 +162,10 @@ TEST_F(RadioHidlTest, changeIccPin2ForApp) {
             EXPECT_EQ(std::cv_status::no_timeout, wait());
             EXPECT_EQ(serial, radioRsp->rspInfo.serial);
             EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
-            ASSERT_TRUE(CheckAnyOfErrors(
-                radioRsp->rspInfo.error,
-                {RadioError::PASSWORD_INCORRECT, RadioError::REQUEST_NOT_SUPPORTED}));
+            ASSERT_TRUE(
+                CheckAnyOfErrors(radioRsp->rspInfo.error,
+                                 {RadioError::PASSWORD_INCORRECT, RadioError::REQUEST_NOT_SUPPORTED,
+                                  RadioError::SIM_PUK2}));
         }
     }
 }
