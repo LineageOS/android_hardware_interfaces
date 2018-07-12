5fd0ccf7a8402991a70ebd55ae5aad3a41172b06
sqian <shuoq@google.com>
2018-07-12 16:30:24 -0700

    Add REQUEST_NOT_SUPPORTED for pin and pin2 Icc VTS
    Bug: 111222834
    Test: sanity
    Change-Id: Ia3b96e83d6173c2c80bdbf7763a876883b167af9
    Merged-In: Ia3b96e83d6173c2c80bdbf7763a876883b167af9
    (cherry picked from commit c0a06f34c88cf9dc017be6178113b35c3c7fc67e)

--- radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
@@ -44,7 +44,9 @@ TEST_F(RadioHidlTest, supplyIccPinForApp) {
             EXPECT_EQ(std::cv_status::no_timeout, wait());
             EXPECT_EQ(serial, radioRsp->rspInfo.serial);
             EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
-            EXPECT_EQ(RadioError::PASSWORD_INCORRECT, radioRsp->rspInfo.error);
+            ASSERT_TRUE(CheckAnyOfErrors(
+                radioRsp->rspInfo.error,
+                {RadioError::PASSWORD_INCORRECT, RadioError::REQUEST_NOT_SUPPORTED}));
         }
     }
 }
@@ -91,8 +93,10 @@ TEST_F(RadioHidlTest, supplyIccPin2ForApp) {
             EXPECT_EQ(std::cv_status::no_timeout, wait());
             EXPECT_EQ(serial, radioRsp->rspInfo.serial);
             EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
-            ASSERT_TRUE(CheckAnyOfErrors(radioRsp->rspInfo.error,
-                                         {RadioError::PASSWORD_INCORRECT, RadioError::SIM_PUK2}));
+            ASSERT_TRUE(
+                CheckAnyOfErrors(radioRsp->rspInfo.error,
+                                 {RadioError::PASSWORD_INCORRECT, RadioError::REQUEST_NOT_SUPPORTED,
+                                  RadioError::SIM_PUK2}));
         }
     }
 }
