69300f89fdbba4d8d50fe03028bf73eabe215221
Sean Hoyt <seanhoyt963@gmail.com>
2018-07-17 08:37:26 +0200

    DNM: support legacy mtk bt hals
    Change-Id: I04e895b3dd50b54ed7f831dc3aeb5a378fb758a5

--- bluetooth/1.0/default/h4_protocol.cc
+++ bluetooth/1.0/default/h4_protocol.cc
@@ -19,6 +19,7 @@
 #define LOG_TAG "android.hardware.bluetooth-hci-h4"
 
 #include <errno.h>
+#include <assert.h>
 #include <fcntl.h>
 #include <log/log.h>
 #include <sys/uio.h>
@@ -30,20 +31,12 @@ namespace bluetooth {
 namespace hci {
 
 size_t H4Protocol::Send(uint8_t type, const uint8_t* data, size_t length) {
-  struct iovec iov[] = {{&type, sizeof(type)},
-                        {const_cast<uint8_t*>(data), length}};
-  ssize_t ret = 0;
-  do {
-    ret = TEMP_FAILURE_RETRY(writev(uart_fd_, iov, sizeof(iov) / sizeof(iov[0])));
-  } while (-1 == ret && EAGAIN == errno);
-
-  if (ret == -1) {
-    ALOGE("%s error writing to UART (%s)", __func__, strerror(errno));
-  } else if (ret < static_cast<ssize_t>(length + 1)) {
-    ALOGE("%s: %d / %d bytes written - something went wrong...", __func__,
-          static_cast<int>(ret), static_cast<int>(length + 1));
-  }
-  return ret;
+  std::unique_ptr<uint8_t[]> adapted_data(new uint8_t[length+1]);
+  uint8_t* p_data = adapted_data.get();
+  *(p_data) = type;
+  memcpy(++p_data, data, length);
+  int rv = WriteSafely(uart_fd_, --p_data, length+1);
+  return rv;
 }
 
 void H4Protocol::OnPacketReady() {
