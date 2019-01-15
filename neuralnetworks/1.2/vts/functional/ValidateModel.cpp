b35eb1eabcd7a8735723a272d0bd5371fd010d7d
Lev Proleev <levp@google.com>
2019-01-15 17:53:46 +0000

    mutateOperationOperandTypeSkip for new GROUPED_CONV_2D variant
    Bug: 119255406
    Test: Vts NNAPI tests.
    Change-Id: I234727f5abba546c1392f7096b7d411d36566bbf
    Merged-In: I234727f5abba546c1392f7096b7d411d36566bbf
    (cherry picked from commit b0762ccb2f4b9c0cb5710509fcbcda543851ef11)

--- neuralnetworks/1.2/vts/functional/ValidateModel.cpp
+++ neuralnetworks/1.2/vts/functional/ValidateModel.cpp
@@ -328,6 +328,7 @@ static bool mutateOperationOperandTypeSkip(size_t operand, OperandType type, con
         // - RANDOM_MULTINOMIAL's argument can be either TENSOR_FLOAT16 or TENSOR_FLOAT32.
         // - CONV_2D filter type (arg 1) can be QUANT8_ASYMM or QUANT8_SYMM_PER_CHANNEL
         // - DEPTHWISE_CONV_2D filter type (arg 1) can be QUANT8_ASYMM or QUANT8_SYMM_PER_CHANNEL
+        // - GROUPED_CONV_2D filter type (arg 1) can be QUANT8_ASYMM or QUANT8_SYMM_PER_CHANNEL
         switch (operation.type) {
             case OperationType::LSH_PROJECTION: {
                 if (operand == operation.inputs[1]) {
@@ -347,6 +348,7 @@ static bool mutateOperationOperandTypeSkip(size_t operand, OperandType type, con
                     return true;
                 }
             } break;
+            case OperationType::GROUPED_CONV_2D:
             case OperationType::DEPTHWISE_CONV_2D:
             case OperationType::CONV_2D: {
                 if (operand == 1 && (type == OperandType::TENSOR_QUANT8_ASYMM ||
