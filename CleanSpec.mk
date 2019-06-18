de7d06babb5e1dd29390d4f0c63714794bdb697f
Kevin Rocard <krocard@google.com>
2019-06-18 16:38:16 -0700

    Convert audio HAL service mk to bp and rename the service
    The service supports multiple versions and its name should not be
    version dependant.
    
    For backward compatibility, keep the target name
    android.hardware.audio@2.0-service (32bit only), existing
    PRODUCT_PACKAGES do not have to be updated.
    New products should depend on android.hardware.audio.service:32.
    
    Additionally migrate to Android.bp, dropping support for
    AUDIOSERVER_MULTILIB. Previously the HAL service architecture (32 vs 64)
    would be the same as the audio server. This in not wanted as all android
    audio HAL are 32 bits.
    
    Bug: 121208203
    Test: # Compare following commands before and after patch:
          find $ANDROID_PRODUCT_OUT/vendor -name 'android.hardware.audio*'|xargs file
                adb shell reboot # check device boots to UI and
          # check the HAL restarts after the audioserver is killed
          adb shell ps |grep audio; adb shell killall audioserver; adb shell ps
    Change-Id: I25f41735175a6687a051ca1e5a7fee670eb8115e
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -80,3 +80,4 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/vndk-Q/android.hardwar
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/android.hardware.configstore@1.2.so)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/vndk-Q/android.hardware.configstore@1.2.so)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/android.hardware.configstore@1.2.so)
+$(call add-clean-step, rm -f $(PRODUCT_OUT)/etc/init/android.hardware.audio@2.0-service.rc $(PRODUCT_OUT)/vendor/bin/hw/android.hardware.audio@2.0-service)
