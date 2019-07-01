07a53471c45c53c00ca0fbe1d83b23a3f2c2ba76
sqian <shuoq@google.com>
2019-07-01 13:20:45 -0700

    Hang up EmergencyDial after Trigger in VTS
    The whole test case run emergencyDial.RadioHidlTest_v1_4 first and then
     come after startNetworkScan.RadioHidlTest_v1_4 test case immediately
     (within ~3 secs) the NWscan failure is device in emergency call in
     progress/connected while doing emergencyDial.RadioHidlTest_v1_4 test
    
    Although emergencyDial.RadioHidlTest_v1_4 only care about command/response
     is successful or not and don't care call is established or not, modem do
     care about call condition is in idle/progress/connected state before it
     can do NWscan
    
    When there's dedicated channel established, modem can not perform AP
     request NWscan in parallel as we are single HW modem restriction.
    
    Test: run vts
    Bug: 135595082
    Change-Id: Iecba54b93b96d33b57bc59e00f082ff133d2e57b

--- radio/1.4/vts/functional/radio_response.cpp
+++ radio/1.4/vts/functional/radio_response.cpp
@@ -77,7 +77,9 @@ Return<void> RadioResponse_v1_4::getIMSIForAppResponse(
     return Void();
 }
 
-Return<void> RadioResponse_v1_4::hangupConnectionResponse(const RadioResponseInfo& /*info*/) {
+Return<void> RadioResponse_v1_4::hangupConnectionResponse(const RadioResponseInfo& info) {
+    rspInfo = info;
+    parent_v1_4.notify(info.serial);
     return Void();
 }
 
@@ -729,9 +731,10 @@ Return<void> RadioResponse_v1_4::getIccCardStatusResponse_1_2(
 
 Return<void> RadioResponse_v1_4::getCurrentCallsResponse_1_2(
         const RadioResponseInfo& info,
-        const ::android::hardware::hidl_vec<::android::hardware::radio::V1_2::Call>& /*calls*/) {
+        const ::android::hardware::hidl_vec<::android::hardware::radio::V1_2::Call>& calls) {
     rspInfo = info;
     parent_v1_4.notify(info.serial);
+    currentCalls = calls;
     return Void();
 }
 
