f3ffb8e5d35f72616e94867255e3c89316bbb79c
arttttt <artem-bambalov@yandex.ru>
2019-12-05 22:05:32 +0300

    fence leaking workaround
    Change-Id: Iab9e5004ed370d5d76d182cb4ba2804ed0065ddc

--- graphics/composer/2.1/utils/hwc2on1adapter/HWC2On1Adapter.cpp
+++ graphics/composer/2.1/utils/hwc2on1adapter/HWC2On1Adapter.cpp
@@ -1294,7 +1294,8 @@ Error HWC2On1Adapter::Display::set(hwc_display_contents_1& hwcContents) {
     auto& clientTargetLayer = hwcContents.hwLayers[numLayers - 1];
     if (clientTargetLayer.compositionType == HWC_FRAMEBUFFER_TARGET) {
         clientTargetLayer.handle = mClientTarget.getBuffer();
-        clientTargetLayer.acquireFenceFd = mClientTarget.getFence();
+        close(mClientTarget.getFence());
+        clientTargetLayer.acquireFenceFd = -1;
     } else {
         ALOGE("[%" PRIu64 "] set: last HWC layer wasn't FRAMEBUFFER_TARGET",
                 mId);
