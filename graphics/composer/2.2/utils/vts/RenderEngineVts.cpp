02ee09f588b7363a559a38ea4c6375c686e8b6c1
Ana Krulec <akrulec@google.com>
2020-02-22 22:00:23 -0800

    SF: RenderEngine->drawLayers takes Graphic Buffer instead of ANativeWindow
    see ag/10392154 for more information
    
    Test: RenderEngineVts
    Change-Id: I75198d2d9fa43013d6497c1c2a393fa3d62c7708

--- graphics/composer/2.2/utils/vts/RenderEngineVts.cpp
+++ graphics/composer/2.2/utils/vts/RenderEngineVts.cpp
@@ -69,9 +69,8 @@ void TestRenderEngine::drawLayers() {
                    [](renderengine::LayerSettings& settings) -> renderengine::LayerSettings* {
                        return &settings;
                    });
-    mRenderEngine->drawLayers(mDisplaySettings, compositionLayerPointers,
-                              mGraphicBuffer->getNativeBuffer(), true, std::move(bufferFence),
-                              &readyFence);
+    mRenderEngine->drawLayers(mDisplaySettings, compositionLayerPointers, mGraphicBuffer, true,
+                              std::move(bufferFence), &readyFence);
     int fd = readyFence.release();
     if (fd != -1) {
         ASSERT_EQ(0, sync_wait(fd, -1));
