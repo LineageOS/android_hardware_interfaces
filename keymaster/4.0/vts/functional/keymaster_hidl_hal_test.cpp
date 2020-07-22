48f67e8438570eaaba8a4a5e99241ab9fd52293f
Gaurav Kashyap <quic_gaurkash@quicinc.com>
2020-07-22 11:14:44 -0700

    Disable the test TooManyOperations in keymaster VTS
    The test fails on devices because an unknown
    client starts a keymaster BEGIN operation during
    bootup but does not finish it. This affects the
    keymaster hardware implementation's capability
    to support the maximum possible operations while
    running this test.
    
    Bug: 154801042
    
    Change-Id: Ib6adc6c28ebe76ddfdc2c66cd17cf78c04e5b468

--- keymaster/4.0/vts/functional/keymaster_hidl_hal_test.cpp
+++ keymaster/4.0/vts/functional/keymaster_hidl_hal_test.cpp
@@ -4537,7 +4537,7 @@ using ClearOperationsTest = KeymasterHidlTest;
  * that aborting the operations clears the operations.
  *
  */
-TEST_P(ClearOperationsTest, TooManyOperations) {
+TEST_P(ClearOperationsTest, DISABLED_TooManyOperations) {
     ASSERT_EQ(ErrorCode::OK, GenerateKey(AuthorizationSetBuilder()
                                              .Authorization(TAG_NO_AUTH_REQUIRED)
                                              .RsaEncryptionKey(2048, 65537)
