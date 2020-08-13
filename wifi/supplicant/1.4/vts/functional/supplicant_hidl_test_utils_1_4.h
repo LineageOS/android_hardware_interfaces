5118e4f043a68e46f2478d15f05b5067cd452a75
Kai Shi <kaishi@google.com>
2020-08-12 18:56:43 -0700

    wifi: Support 11b only mode
    upgrade @1.4::ConnectionCapabilities to include legacyMode, update
    corresponding 1.3 VTS test and add new 1.4 VTS test files
    
    Bug: 163912712
    Test: atest VtsHalWifiSupplicantV1_0TargetTest \
                VtsHalWifiSupplicantV1_1TargetTest \
    	    VtsHalWifiSupplicantV1_2TargetTest \
    	    VtsHalWifiSupplicantV1_3TargetTest \
    	    VtsHalWifiSupplicantV1_4TargetTest
    Test: manual test with 11b only AP to confirm 11b mode is detected and
    passed to throughput predictor correctly.
    
    Change-Id: Id4fd567401289091a5d74a770e759eee326c3873

---
+++ wifi/supplicant/1.4/vts/functional/supplicant_hidl_test_utils_1_4.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SUPPLICANT_HIDL_TEST_UTILS_1_4_H
+#define SUPPLICANT_HIDL_TEST_UTILS_1_4_H
+
+#include <android/hardware/wifi/supplicant/1.4/ISupplicant.h>
+#include <android/hardware/wifi/supplicant/1.4/ISupplicantStaIface.h>
+
+android::sp<android::hardware::wifi::supplicant::V1_4::ISupplicantStaIface>
+getSupplicantStaIface_1_4(
+    const android::sp<android::hardware::wifi::supplicant::V1_4::ISupplicant>&
+        supplicant);
+android::sp<android::hardware::wifi::supplicant::V1_4::ISupplicant>
+getSupplicant_1_4(const std::string& supplicant_instance_name, bool isP2pOn);
+
+#endif /* SUPPLICANT_HIDL_TEST_UTILS_1_4_H */
