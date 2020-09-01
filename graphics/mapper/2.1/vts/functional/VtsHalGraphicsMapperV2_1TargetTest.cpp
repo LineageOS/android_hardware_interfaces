3c9a3a0cd11950ec9deb16b6693ecc5adb9fa5a6
Chris Forbes <chrisforbes@google.com>
2020-08-31 17:14:39 -0700

    Mark GraphicsMapperHidlTest as allowing not being instantiated
    Only the variant(s) actually implemented by the device will be
    instantiated.
    
    This is consistent with tests for other versioned or optional HALs.
    
    Bug: b/167222309
    Test: atest VtsHalGraphicsMapperV2_0TargetTest:GoogleTestVerification#UninstantiatedParameterizedTestSuite<GraphicsMapperHidlTest> -- --abi arm64-v8a
    Change-Id: I4f1bfbad47bdbf75578ec48a3a56fb5351e7729a

--- graphics/mapper/2.1/vts/functional/VtsHalGraphicsMapperV2_1TargetTest.cpp
+++ graphics/mapper/2.1/vts/functional/VtsHalGraphicsMapperV2_1TargetTest.cpp
@@ -204,6 +204,7 @@ TEST_P(GraphicsMapperHidlTest, CreateDescriptor_2_1Negative) {
     });
 }
 
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(GraphicsMapperHidlTest);
 INSTANTIATE_TEST_CASE_P(
         PerInstance, GraphicsMapperHidlTest,
         testing::Combine(
