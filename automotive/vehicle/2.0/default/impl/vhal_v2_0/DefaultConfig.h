a3b16c2d8e3354d848dfb18826ce89d2bdb6460e
Kai <kwangsudo@google.com>
2020-09-01 21:40:22 -0700

    Add configArray for Gear_Selection in google VHAL
    ConfigArray in Gear_Selection indicates the supported Gear
    
    Bug: 165942804
    Test: build and check value in emulator
    Change-Id: I4567031a072edda550b671bc190934d24d5b5eba

--- automotive/vehicle/2.0/default/impl/vhal_v2_0/DefaultConfig.h
+++ automotive/vehicle/2.0/default/impl/vhal_v2_0/DefaultConfig.h
@@ -674,6 +674,12 @@ const ConfigDeclaration kVehicleProperties[]{
                          .prop = toInt(VehicleProperty::GEAR_SELECTION),
                          .access = VehiclePropertyAccess::READ,
                          .changeMode = VehiclePropertyChangeMode::ON_CHANGE,
+                         .configArray = {(int)VehicleGear::GEAR_PARK,
+                                         (int)VehicleGear::GEAR_NEUTRAL,
+                                         (int)VehicleGear::GEAR_REVERSE,
+                                         (int)VehicleGear::GEAR_DRIVE, (int)VehicleGear::GEAR_1,
+                                         (int)VehicleGear::GEAR_2, (int)VehicleGear::GEAR_3,
+                                         (int)VehicleGear::GEAR_4, (int)VehicleGear::GEAR_5},
                  },
          .initialValue = {.int32Values = {toInt(VehicleGear::GEAR_PARK)}}},
 
