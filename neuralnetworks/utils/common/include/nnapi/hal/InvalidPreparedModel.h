376005883c5981f1255e52e9a4ed417ab81fce58
Michael Butler <butlermichael@google.com>
2020-11-19 20:46:58 -0800

    Invalidate NN interface objects on cache mismatch
    Currently, if an IDevice object is a DEAD_OBJECT, the runtime attempts
    to re-retrieve the handle to the rebooted IDevice service. If an update
    occurs after the IDevice was originally created, the rebooted IDevice
    object may have different metadata and behavior. This is problematic
    because the original metadata is cached in the runtime. Further, an
    application might have made decisions based on that metadata and
    behavior. (Note that a driver service that is functionally the same but
    has a different underlying implementation such as having more optimized
    code will have different `getVersionString` metadata.) Instead, this CL
    invalidates the IDevice object on cache mismatch, and always returns an
    error if it is used.
    
    Bug: 173081926
    Test: mma
    Change-Id: I805987361c627c32d45e1b7c7aed230376fc66ad
    Merged-In: I805987361c627c32d45e1b7c7aed230376fc66ad
    (cherry picked from commit 5a74c0fb0f23474a89471c49111e5ab526735392)

---
+++ neuralnetworks/utils/common/include/nnapi/hal/InvalidPreparedModel.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_INVALID_PREPARED_MODEL_H
+#define ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_INVALID_PREPARED_MODEL_H
+
+#include <nnapi/IPreparedModel.h>
+#include <nnapi/Result.h>
+#include <nnapi/Types.h>
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+namespace android::hardware::neuralnetworks::utils {
+
+class InvalidPreparedModel final : public nn::IPreparedModel {
+  public:
+    nn::ExecutionResult<std::pair<std::vector<nn::OutputShape>, nn::Timing>> execute(
+            const nn::Request& request, nn::MeasureTiming measure,
+            const nn::OptionalTimePoint& deadline,
+            const nn::OptionalTimeoutDuration& loopTimeoutDuration) const override;
+
+    nn::GeneralResult<std::pair<nn::SyncFence, nn::ExecuteFencedInfoCallback>> executeFenced(
+            const nn::Request& request, const std::vector<nn::SyncFence>& waitFor,
+            nn::MeasureTiming measure, const nn::OptionalTimePoint& deadline,
+            const nn::OptionalTimeoutDuration& loopTimeoutDuration,
+            const nn::OptionalTimeoutDuration& timeoutDurationAfterFence) const override;
+
+    std::any getUnderlyingResource() const override;
+};
+
+}  // namespace android::hardware::neuralnetworks::utils
+
+#endif  // ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_INVALID_PREPARED_MODEL_H
