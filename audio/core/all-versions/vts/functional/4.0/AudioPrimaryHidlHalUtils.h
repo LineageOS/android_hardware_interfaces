893ea216812749d3e22a9d94a8f4d92288431458
Mikhail Naganov <mnaganov@google.com>
2022-02-04 00:00:11 +0000

    audio: Remove IStreamIn@7.1
    This has turned out to be an empty interface. By removing
    it we save about 100 kB in the interface code.
    
    Bug: 217752112
    Test: atest VtsHalAudioV7_1TargetTest
    Change-Id: I2522653600a254ddcf2c4eac8bed1df69ac11d3d

--- audio/core/all-versions/vts/functional/4.0/AudioPrimaryHidlHalUtils.h
+++ audio/core/all-versions/vts/functional/4.0/AudioPrimaryHidlHalUtils.h
@@ -15,9 +15,9 @@
  */
 
 // clang-format off
-#include PATH(android/hardware/audio/FILE_VERSION/IStreamIn.h)
-#include PATH(android/hardware/audio/FILE_VERSION/IStreamOut.h)
+#include PATH(android/hardware/audio/CORE_TYPES_FILE_VERSION/IStreamIn.h)
 #include PATH(android/hardware/audio/CORE_TYPES_FILE_VERSION/types.h)
+#include PATH(android/hardware/audio/FILE_VERSION/IStreamOut.h)
 #include PATH(android/hardware/audio/common/COMMON_TYPES_FILE_VERSION/types.h)
 // clang-format on
 #include <hidl/HidlSupport.h>
