335015939hi/dumbos-platform_hardware_interfaces

AIDL effect: Add queryProcess in IFactory
Bug: 238913361
Test: atest VtsHalAudioEffectTargetTest; atest VtsHalAudioEffectFactoryTargetTest
Change-Id: I5518ceaa0ea744e30803fc23996e2a2b48398024

audio/aidl/aidl_api/android.hardware.audio.effect/current/android/hardware/audio/effect/IFactory.aidl
@@ -35,6 +35,7 @@ package android.hardware.audio.effect;
 @VintfStability
 interface IFactory {
   android.hardware.audio.effect.Descriptor.Identity[] queryEffects(in @nullable android.media.audio.common.AudioUuid type, in @nullable android.media.audio.common.AudioUuid implementation);
+  android.hardware.audio.effect.Process[] queryProcess(in @nullable android.hardware.audio.effect.Process.Type type);
   android.hardware.audio.effect.IEffect createEffect(in android.media.audio.common.AudioUuid implUuid);
   void destroyEffect(in android.hardware.audio.effect.IEffect handle);
 }

audio/aidl/default/EffectFactory.cpp
@@ -73,6 +73,17 @@ ndk::ScopedAStatus Factory::queryEffects(const std::optional<AudioUuid>& in_type
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus Factory::queryProcess(const std::optional<Process::Type>& in_type,
+                                         std::vector<Process>* _aidl_return) {
+    // TODO: implement this with audio_effect.xml.
+    if (in_type.has_value()) {
+        // return all matching process filter
+        LOG(DEBUG) << __func__ << " process type: " << in_type.value().toString();
+    }
+    LOG(DEBUG) << __func__ << " return " << _aidl_return->size();
+    return ndk::ScopedAStatus::ok();
+}
+
 #define RETURN_IF_BINDER_EXCEPTION(functor)                                 \
     {                                                                       \
         binder_exception_t exception = functor;                             \

audio/aidl/vts/EffectFactoryHelper.h
@@ -31,6 +31,7 @@ using aidl::android::hardware::audio::effect::Descriptor;
 using aidl::android::hardware::audio::effect::IEffect;
 using aidl::android::hardware::audio::effect::IFactory;
 using aidl::android::hardware::audio::effect::Parameter;
+using aidl::android::hardware::audio::effect::Process;
 using aidl::android::media::audio::common::AudioUuid;
 
 class EffectFactoryHelper {
@@ -55,6 +56,16 @@ class EffectFactoryHelper {
         ASSERT_NE(mEffectFactory, nullptr);
         EXPECT_IS_OK(mEffectFactory->queryEffects(in_type, in_instance, _aidl_return));
         mIds = *_aidl_return;
+    }
+
+    void QueryProcesses(const std::optional<Process::Type>& in_type,
+                        std::vector<Process>* _aidl_return) {
+        ASSERT_NE(mEffectFactory, nullptr);
+        EXPECT_IS_OK(mEffectFactory->queryProcess(in_type, _aidl_return));
+        // only update the whole list if no filter applied
+        if (!in_type.has_value()) {
+            mProcesses = *_aidl_return;
+        }
     }
 
     void CreateEffects() {
@@ -127,6 +138,7 @@ class EffectFactoryHelper {
     AudioHalBinderServiceUtil binderUtil;
     std::vector<Descriptor::Identity> mIds;
     std::vector<Descriptor::Identity> mCompleteIds;
+    std::vector<Process> mProcesses;
 
     std::map<std::shared_ptr<IEffect>, Descriptor::Identity> mEffectIdMap;
 };

audio/aidl/vts/VtsHalAudioEffectFactoryTargetTest.cpp
@@ -38,6 +38,7 @@ using namespace android;
 
 using aidl::android::hardware::audio::effect::Descriptor;
 using aidl::android::hardware::audio::effect::IFactory;
+using aidl::android::hardware::audio::effect::Process;
 using aidl::android::media::audio::common::AudioUuid;
 
 /// Effect factory testing.
@@ -220,6 +221,12 @@ TEST_P(EffectFactoryTest, CreateRestartAndCreateDestroy) {
     EXPECT_EQ(effectMap.size(), numIds);
     mFactory.DestroyEffects();
     EXPECT_EQ(effectMap.size(), 0UL);
+}
+
+TEST_P(EffectFactoryTest, QueryProcess) {
+    std::vector<Process> processes;
+    mFactory.QueryProcesses(std::nullopt, &processes);
+    // TODO: verify the number of process in example implementation after audio_effects.xml migrated
 }
 
 INSTANTIATE_TEST_SUITE_P(EffectFactoryTest, EffectFactoryTest,

LineageOS/android_hardware_interfaces

audio/aidl/aidl_api/android.hardware.audio.effect/current/android/hardware/audio/effect/IFactory.aidl
@@ -35,6 +35,7 @@ package android.hardware.audio.effect;
 @VintfStability
 interface IFactory {
   android.hardware.audio.effect.Descriptor.Identity[] queryEffects(in @nullable android.media.audio.common.AudioUuid type, in @nullable android.media.audio.common.AudioUuid implementation);
+  android.hardware.audio.effect.Processing[] queryProcessing(in @nullable android.hardware.audio.effect.Processing.Type type);
   android.hardware.audio.effect.IEffect createEffect(in android.media.audio.common.AudioUuid implUuid);
   void destroyEffect(in android.hardware.audio.effect.IEffect handle);
 }

audio/aidl/default/EffectFactory.cpp
@@ -73,6 +73,17 @@ ndk::ScopedAStatus Factory::queryEffects(const std::optional<AudioUuid>& in_type
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus Factory::queryProcessing(const std::optional<Processing::Type>& in_type,
+                                            std::vector<Processing>* _aidl_return) {
+    // TODO: implement this with audio_effect.xml.
+    if (in_type.has_value()) {
+        // return all matching process filter
+        LOG(DEBUG) << __func__ << " process type: " << in_type.value().toString();
+    }
+    LOG(DEBUG) << __func__ << " return " << _aidl_return->size();
+    return ndk::ScopedAStatus::ok();
+}
+
 #define RETURN_IF_BINDER_EXCEPTION(functor)                                 \
     {                                                                       \
         binder_exception_t exception = functor;                             \

audio/aidl/vts/EffectFactoryHelper.h
@@ -31,6 +31,7 @@ using aidl::android::hardware::audio::effect::Descriptor;
 using aidl::android::hardware::audio::effect::IEffect;
 using aidl::android::hardware::audio::effect::IFactory;
 using aidl::android::hardware::audio::effect::Parameter;
+using aidl::android::hardware::audio::effect::Processing;
 using aidl::android::media::audio::common::AudioUuid;
 
 class EffectFactoryHelper {
@@ -55,6 +56,16 @@ class EffectFactoryHelper {
         ASSERT_NE(mEffectFactory, nullptr);
         EXPECT_IS_OK(mEffectFactory->queryEffects(in_type, in_instance, _aidl_return));
         mIds = *_aidl_return;
+    }
+
+    void QueryProcessing(const std::optional<Processing::Type>& in_type,
+                         std::vector<Processing>* _aidl_return) {
+        ASSERT_NE(mEffectFactory, nullptr);
+        EXPECT_IS_OK(mEffectFactory->queryProcessing(in_type, _aidl_return));
+        // only update the whole list if no filter applied
+        if (!in_type.has_value()) {
+            mProcesses = *_aidl_return;
+        }
     }
 
     void CreateEffects() {
@@ -127,6 +138,7 @@ class EffectFactoryHelper {
     AudioHalBinderServiceUtil binderUtil;
     std::vector<Descriptor::Identity> mIds;
     std::vector<Descriptor::Identity> mCompleteIds;
+    std::vector<Processing> mProcesses;
 
     std::map<std::shared_ptr<IEffect>, Descriptor::Identity> mEffectIdMap;
 };

audio/aidl/vts/VtsHalAudioEffectFactoryTargetTest.cpp
@@ -38,6 +38,7 @@ using namespace android;
 
 using aidl::android::hardware::audio::effect::Descriptor;
 using aidl::android::hardware::audio::effect::IFactory;
+using aidl::android::hardware::audio::effect::Processing;
 using aidl::android::media::audio::common::AudioUuid;
 
 /// Effect factory testing.
@@ -220,6 +221,12 @@ TEST_P(EffectFactoryTest, CreateRestartAndCreateDestroy) {
     EXPECT_EQ(effectMap.size(), numIds);
     mFactory.DestroyEffects();
     EXPECT_EQ(effectMap.size(), 0UL);
+}
+
+TEST_P(EffectFactoryTest, QueryProcess) {
+    std::vector<Processing> processing;
+    mFactory.QueryProcessing(std::nullopt, &processing);
+    // TODO: verify the number of process in example implementation after audio_effects.xml migrated
 }
 
 INSTANTIATE_TEST_SUITE_P(EffectFactoryTest, EffectFactoryTest,