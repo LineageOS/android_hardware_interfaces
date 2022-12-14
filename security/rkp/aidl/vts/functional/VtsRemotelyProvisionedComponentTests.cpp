335015939hi/dumbos-platform_hardware_interfaces

Add VSR enforcement testing for IRPC versioning.
Check the VSR API level for the device under test and ensure that the
appropriate HAL version for the IRemotelyProvisionedComponent interface
is present. E.g. a VSR 13 chipset should have IRPC v2 or newer.

Bug: 251185719
Test: atest VtsHalRemotelyProvisionedComponentTargetTest
Change-Id: I136cac27953d1d5acaa45510ad9efec2a75d8038

security/keymint/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
@@ -797,4 +797,29 @@ TEST_P(CertificateRequestV2Test, CertificateRequestV1Removed) {
 
 INSTANTIATE_REM_PROV_AIDL_TEST(CertificateRequestV2Test);
 
+using VsrRequirementTest = VtsRemotelyProvisionedComponentTests;
+
+TEST_P(VsrRequirementTest, Vsr13Test) {
+    RpcHardwareInfo hwInfo;
+    ASSERT_TRUE(provisionable_->getHardwareInfo(&hwInfo).isOk());
+    int vsr_api_level = get_vsr_api_level();
+    if (vsr_api_level < 32) {
+        GTEST_SKIP() << "Applies only to VSR API level 32 or newer, this device is: "
+                     << vsr_api_level;
+    }
+    switch (vsr_api_level) {
+        case 32:
+            EXPECT_GE(hwInfo.versionNumber, 1) << "VSR 12 requires IRPC v1 or newer.";
+            break;
+        case 33:
+            EXPECT_GE(hwInfo.versionNumber, 2) << "VSR 13 requires IRPC v2 or newer.";
+            break;
+        default:
+            EXPECT_GE(hwInfo.versionNumber, 3) << "VSR 14+ requires IRPC v3 or newer.";
+            break;
+    }
+}
+
+INSTANTIATE_KEYMINT_AIDL_TEST(VsrRequirementTest);
+
 }  // namespace aidl::android::hardware::security::keymint::test

LineageOS/android_hardware_interfaces

security/rkp/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
@@ -783,4 +783,20 @@ TEST_P(CertificateRequestV2Test, NonEmptyRequest_testKeyInProdCert) {
 
 INSTANTIATE_REM_PROV_AIDL_TEST(CertificateRequestV2Test);
 
+using VsrRequirementTest = VtsRemotelyProvisionedComponentTests;
+
+INSTANTIATE_REM_PROV_AIDL_TEST(VsrRequirementTest);
+
+TEST_P(VsrRequirementTest, VsrEnforcementTest) {
+    RpcHardwareInfo hwInfo;
+    ASSERT_TRUE(provisionable_->getHardwareInfo(&hwInfo).isOk());
+    int vsr_api_level = get_vsr_api_level();
+    if (vsr_api_level < 34) {
+        GTEST_SKIP() << "Applies only to VSR API level 34 or newer, this device is: "
+                     << vsr_api_level;
+    }
+    EXPECT_GE(hwInfo.versionNumber, 3)
+            << "VSR 14+ requires IRemotelyProvisionedComponent v3 or newer.";
+}
+
 }  // namespace aidl::android::hardware::security::keymint::test