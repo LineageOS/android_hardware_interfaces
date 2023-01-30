335015939hi/dumbos-platform_hardware_interfaces

Add TV tuner audio preselection support
DVB MPEG-2 transport streams can include audio preselection descriptors
for next-generation audio (NGA) as specified in ETSI EN 300 468. Add
support for audio preselections in DemuxFilterMediaEvent.

Note that, when an audio preselection descriptor is present for a NGA
stream, receivers are required to ignore (supplementary) audio
descriptors (see Annex M.2).

Change-Id: I5b6d9a86aa74feb87879125f57ca36d7e71bbdf4

tv/tuner/aidl/aidl_api/android.hardware.tv.tuner/current/android/hardware/tv/tuner/AudioPreselection.aidl
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,20 +34,13 @@
 package android.hardware.tv.tuner;
 /* @hide */
 @VintfStability
-parcelable DemuxFilterMediaEvent {
-  int streamId;
-  boolean isPtsPresent;
-  long pts;
-  boolean isDtsPresent;
-  long dts;
-  long dataLength;
-  long offset;
-  android.hardware.common.NativeHandle avMemory;
-  boolean isSecureMemory;
-  long avDataId;
-  int mpuSequenceNumber;
-  boolean isPesPrivateData;
-  android.hardware.tv.tuner.DemuxFilterMediaEventExtraMetaData extraMetaData;
-  android.hardware.tv.tuner.DemuxFilterScIndexMask scIndexMask;
-  android.hardware.tv.tuner.AudioPreselection[] audioPreselections = {};
+parcelable AudioPreselection {
+  int preselectionId;
+  int programId;
+  android.hardware.tv.tuner.AudioPreselectionMessageDescriptor[] messageDescriptors;
+  String language;
+  android.hardware.tv.tuner.AudioPreselectionRenderingIndicationType renderingIndication;
+  boolean hasAudioDescription;
+  boolean hasSpokenSubtitles;
+  boolean hasDialogueEnhancement;
 }

LineageOS/android_hardware_interfaces

Bug: 264812332
Test: `atest VtsHalTvTunerTargetTest` on cf_x86_tv-userdebug

tv/tuner/aidl/aidl_api/android.hardware.tv.tuner/current/android/hardware/tv/tuner/AudioPreselection.aidl
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,8 +34,12 @@
 package android.hardware.tv.tuner;
 /* @hide */
 @VintfStability
-union DemuxFilterMediaEventExtraMetaData {
-  boolean noinit;
-  android.hardware.tv.tuner.AudioExtraMetaData audio;
-  android.hardware.tv.tuner.AudioPresentation[] audioPresentations = {};
+parcelable AudioPreselection {
+  int preselectionId;
+  android.hardware.tv.tuner.AudioPreselectionLabel[] labels;
+  String language;
+  android.hardware.tv.tuner.AudioPreselectionRenderingIndicationType renderingIndication;
+  boolean hasAudioDescription;
+  boolean hasSpokenSubtitles;
+  boolean hasDialogueEnhancement;
 }