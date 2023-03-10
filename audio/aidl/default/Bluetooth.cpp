b4f8e67e1c1b195f69eace6ba7dbbb4e1569e506
Mikhail Naganov <mnaganov@google.com>
2023-03-10 09:19:35 -0800

    audio: Add offload reconfiguration event to IBluetoothLe
    Since BT LE can also have offloaded codecs, it might need
    a reconfiguration event similar to the one that BT A2DP
    receives.
    
    Bug: 272658632
    Test: atest VtsHalAudioCoreTargetTest
    Change-Id: I04fb7c99f0457f87e1f4aaf7e77165317163387b

--- audio/aidl/default/Bluetooth.cpp
+++ audio/aidl/default/Bluetooth.cpp
@@ -117,4 +117,17 @@ ndk::ScopedAStatus BluetoothLe::setEnabled(bool in_enabled) {
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus BluetoothLe::supportsOffloadReconfiguration(bool* _aidl_return) {
+    *_aidl_return = true;
+    LOG(DEBUG) << __func__ << ": returning " << *_aidl_return;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus BluetoothLe::reconfigureOffload(
+        const std::vector<::aidl::android::hardware::audio::core::VendorParameter>& in_parameters
+                __unused) {
+    LOG(DEBUG) << __func__ << ": " << ::android::internal::ToString(in_parameters);
+    return ndk::ScopedAStatus::ok();
+}
+
 }  // namespace aidl::android::hardware::audio::core
