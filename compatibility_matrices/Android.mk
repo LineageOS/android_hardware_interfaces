d1df10178d315032af6914eb9ced3022b29a0afb
Yifan Hong <elsk@google.com>
2023-04-12 14:20:48 -0700

    Add compatibility matrix for V.
    Copied from U matrix. Also include the radio.satellite HAL.
    
    Test: TH
    Bug: 266741352
    Change-Id: Ic42e51b616782a2763eb80e1cad5ca776cc079d4

--- compatibility_matrices/Android.mk
+++ compatibility_matrices/Android.mk
@@ -103,6 +103,7 @@ my_system_matrix_deps := \
     framework_compatibility_matrix.6.xml \
     framework_compatibility_matrix.7.xml \
     framework_compatibility_matrix.8.xml \
+    framework_compatibility_matrix.9.xml \
     framework_compatibility_matrix.device.xml \
 
 my_framework_matrix_deps += \
