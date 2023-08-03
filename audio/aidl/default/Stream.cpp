335015939hi/dumbos-platform_hardware_interfaces

aosp aidl bluetooth parameter support
Bug:
Test: Build

Change-Id: If1820018d5a6750eed0d0b486e15fc7c717aa11c

audio/aidl/default/Stream.cpp
@@ -696,6 +696,11 @@ ndk::ScopedAStatus StreamCommonImpl::setConnectedDevices(
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus StreamCommonImpl::onBluetoothParametersUpdated() {
+    LOG(DEBUG) << __func__;
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
 namespace {
 static std::map<AudioDevice, std::string> transformMicrophones(
         const std::vector<MicrophoneInfo>& microphones) {

LineageOS/android_hardware_interfaces

Bug: 228804498
Test: atest VtsHalAudioCoreTargetTest
(cherry picked from commit 18f0d5121af3701decc0b2b306fbe5e8a2db7265)
Merged-In: If1820018d5a6750eed0d0b486e15fc7c717aa11c

audio/aidl/default/Stream.cpp
@@ -723,6 +723,11 @@ ndk::ScopedAStatus StreamCommonImpl::setConnectedDevices(
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus StreamCommonImpl::bluetoothParametersUpdated() {
+    LOG(DEBUG) << __func__;
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
 namespace {
 static std::map<AudioDevice, std::string> transformMicrophones(
         const std::vector<MicrophoneInfo>& microphones) {