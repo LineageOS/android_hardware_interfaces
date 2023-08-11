3e74f0b928f9fdf16a78a0507c06f2d35f68f70c
Tomasz Wasilczyk <twasilczyk@google.com>
2023-08-11 16:04:23 +0000

    Use String8/16 c_str [cas]
    Bug: 295394788
    Test: make checkbuild
    Change-Id: I1faccac8711d5cccee70443548940d99804c7c2a

--- cas/aidl/default/FactoryLoader.h
+++ cas/aidl/default/FactoryLoader.h
@@ -86,10 +86,10 @@ bool FactoryLoader<T>::findFactoryForScheme(int32_t CA_system_id,
 #else
     String8 dirPath("/vendor/lib/mediacas");
 #endif
-    DIR* pDir = opendir(dirPath.string());
+    DIR* pDir = opendir(dirPath.c_str());
 
     if (pDir == NULL) {
-        ALOGE("Failed to open plugin directory %s", dirPath.string());
+        ALOGE("Failed to open plugin directory %s", dirPath.c_str());
         return false;
     }
 
@@ -123,10 +123,10 @@ bool FactoryLoader<T>::enumeratePlugins(vector<AidlCasPluginDescriptor>* results
 #else
     String8 dirPath("/vendor/lib/mediacas");
 #endif
-    DIR* pDir = opendir(dirPath.string());
+    DIR* pDir = opendir(dirPath.c_str());
 
     if (pDir == NULL) {
-        ALOGE("Failed to open plugin directory %s", dirPath.string());
+        ALOGE("Failed to open plugin directory %s", dirPath.c_str());
         return false;
     }
 
