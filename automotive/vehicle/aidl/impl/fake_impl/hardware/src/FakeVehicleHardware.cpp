6791803d4ad568c8ba66d20199e84746f412df23
terryguan <terryguan@google.com>
2023-08-22 17:46:12 -0700

    Add buffer result for generating vendor configs
    Add buffers to verify the success of generating and restoring vendor
    configs.
    
    Bug: 281835821
    Test: atest android.car.apitest.CarPropertyManagerTest
    Change-Id: Ic3cb8bf3e606847ff55d719900ad0b764b56aaf4

--- automotive/vehicle/aidl/impl/fake_impl/hardware/src/FakeVehicleHardware.cpp
+++ automotive/vehicle/aidl/impl/fake_impl/hardware/src/FakeVehicleHardware.cpp
@@ -1015,9 +1015,11 @@ DumpResult FakeVehicleHardware::dump(const std::vector<std::string>& options) {
     } else if (EqualsIgnoreCase(option, "--genTestVendorConfigs")) {
         mAddExtraTestVendorConfigs = true;
         result.refreshPropertyConfigs = true;
+        result.buffer = "successfully generated vendor configs";
     } else if (EqualsIgnoreCase(option, "--restoreVendorConfigs")) {
         mAddExtraTestVendorConfigs = false;
         result.refreshPropertyConfigs = true;
+        result.buffer = "successfully restored vendor configs";
     } else {
         result.buffer = StringPrintf("Invalid option: %s\n", option.c_str());
     }
