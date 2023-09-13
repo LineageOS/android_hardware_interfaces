7449841dc56312ef54fbbec17a9d5547348d7478
Jaideep Sharma <quic_jaidshar@quicinc.com>
2023-09-13 15:25:25 +0530

    audio: Add VTS execution tracer
    Add Execution listener in VTS tests to get information
    about test execution steps like, start, stop and failure messages
    in logcat message.
    
    Bug: 287168985
    Test: run vts-hal-audio
    
    Change-Id: Ie69a7bf78076c17d7b623d75748fa0fdbdba6b1e

--- audio/aidl/vts/VtsHalAGC1TargetTest.cpp
+++ audio/aidl/vts/VtsHalAGC1TargetTest.cpp
@@ -28,6 +28,7 @@ using aidl::android::hardware::audio::effect::getEffectTypeUuidAutomaticGainCont
 using aidl::android::hardware::audio::effect::IEffect;
 using aidl::android::hardware::audio::effect::IFactory;
 using aidl::android::hardware::audio::effect::Parameter;
+using android::hardware::audio::common::testing::detail::TestExecutionTracer;
 
 enum ParamName {
     PARAM_INSTANCE_NAME,
@@ -189,6 +190,7 @@ GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(AGC1ParamTest);
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    ::testing::UnitTest::GetInstance()->listeners().Append(new TestExecutionTracer());
     ABinderProcess_setThreadPoolMaxThreadCount(1);
     ABinderProcess_startThreadPool();
     return RUN_ALL_TESTS();
