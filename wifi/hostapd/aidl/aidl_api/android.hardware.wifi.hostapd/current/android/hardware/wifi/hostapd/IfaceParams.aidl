335015939hi/dumbos-platform_hardware_interfaces

Add list of OuiKeyedData to the Hostapd AIDL IfaceParams type.
Requires update to the latest compatibility
matrix to upgrade the Hostapd HAL to V2.

Bug: 296069900
Test: m
Change-Id: I125937857fbc5fee0815369532e502da4c454a53

wifi/hostapd/aidl/aidl_api/android.hardware.wifi.hostapd/current/android/hardware/wifi/hostapd/IfaceParams.aidl
@@ -37,4 +37,5 @@ parcelable IfaceParams {
   String name;
   android.hardware.wifi.hostapd.HwModeParams hwModeParams;
   android.hardware.wifi.hostapd.ChannelParams[] channelParams;
+  @nullable android.hardware.wifi.common.OuiKeyedData[] mVendorData;
 }

LineageOS/android_hardware_interfaces

wifi/hostapd/aidl/aidl_api/android.hardware.wifi.hostapd/current/android/hardware/wifi/hostapd/IfaceParams.aidl
@@ -37,4 +37,5 @@ parcelable IfaceParams {
   String name;
   android.hardware.wifi.hostapd.HwModeParams hwModeParams;
   android.hardware.wifi.hostapd.ChannelParams[] channelParams;
+  @nullable android.hardware.wifi.common.OuiKeyedData[] vendorData;
 }