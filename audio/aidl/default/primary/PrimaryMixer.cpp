a3d064cd14d49f07aa768c2d55b0c9fd527a598e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-25 17:23:37 +0800

    audio: aidl: default: primary: Read alsa card and device from prop
    Change-Id: I33328cadd96ca932d39e174c6128a449bd6b95df

--- audio/aidl/default/primary/PrimaryMixer.cpp
+++ audio/aidl/default/primary/PrimaryMixer.cpp
@@ -16,14 +16,42 @@
 
 #define LOG_TAG "AHAL_PrimaryMixer"
 
+#include <android-base/properties.h>
+
 #include "PrimaryMixer.h"
 
+using android::base::GetIntProperty;
+
 namespace aidl::android::hardware::audio::core::primary {
+
+namespace {
+
+static constexpr int kAlsaCard = 0;
+static constexpr int kAlsaDevice = 0;
+static constexpr char kAlsaCardProp[] = "persist.vendor.audio.primary.alsa_card";
+static constexpr char kAlsaDeviceProp[] = "persist.vendor.audio.primary.alsa_device";
+
+}  // namespace
+
+PrimaryMixer::PrimaryMixer() : alsa::Mixer(getAlsaCard()) {}
 
 // static
 PrimaryMixer& PrimaryMixer::getInstance() {
     static PrimaryMixer gInstance;
     return gInstance;
 }
+
+int PrimaryMixer::getAlsaCard() {
+    if (AlsaCard == -1) AlsaCard = GetIntProperty(kAlsaCardProp, kAlsaCard);
+    return AlsaCard;
+}
+
+int PrimaryMixer::getAlsaDevice() {
+    if (AlsaDevice == -1) AlsaDevice = GetIntProperty(kAlsaDeviceProp, kAlsaDevice);
+    return AlsaDevice;
+}
+
+int PrimaryMixer::AlsaCard = -1;
+int PrimaryMixer::AlsaDevice = -1;
 
 }  // namespace aidl::android::hardware::audio::core::primary
