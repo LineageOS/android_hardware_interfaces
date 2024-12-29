0014d890386345d8a35342b5c7f6e83257f4cd04
Aaron Kling <webgeek1234@gmail.com>
2024-12-28 21:16:57 -0600

    Conditionally add back mapper to composer res 2.1
    Change-Id: I34b53436eb680cd86499669ff5b0fac417877853

--- graphics/composer/2.1/utils/resources/include/composer-resources/2.1/ComposerResources.h
+++ graphics/composer/2.1/utils/resources/include/composer-resources/2.1/ComposerResources.h
@@ -27,6 +27,9 @@
 
 #include <android/hardware/graphics/composer/2.1/types.h>
 
+#ifdef OLD_MAPPER
+#include <android/hardware/graphics/mapper/2.0/IMapper.h>
+#endif
 #include <log/log.h>
 
 namespace android {
@@ -40,7 +43,9 @@ namespace hal {
 // wrapper for IMapper to import buffers and sideband streams
 class ComposerHandleImporter {
   public:
+#ifndef OLD_MAPPER
     ComposerHandleImporter();
+#endif
     bool init();
 
     Error importBuffer(const native_handle_t* rawHandle, const native_handle_t** outBufferHandle);
@@ -49,7 +54,11 @@ class ComposerHandleImporter {
     void freeStream(const native_handle_t* streamHandle);
 
   private:
+#ifndef OLD_MAPPER
     GraphicBufferMapper& mMapper;
+#else
+    sp<mapper::V2_0::IMapper> mMapper2;
+#endif
 };
 
 class ComposerHandleCache {
