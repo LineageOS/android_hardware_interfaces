c0be99b092ca19974d7d9fd32fe9c5441ebe6059
Danny Trunk <dtrunk90@gmail.com>
2025-03-31 22:18:52 +0200

    Add compatibility for IRadio 1.4
    Change-Id: I9bfa3842f71a87268da640f34553a62ada2f4ea8

--- radio/aidl/compat/libradiocompat/modem/RadioModem.cpp
+++ radio/aidl/compat/libradiocompat/modem/RadioModem.cpp
@@ -33,19 +33,19 @@ std::shared_ptr<aidl::IRadioModemResponse> RadioModem::respond() {
 
 ScopedAStatus RadioModem::enableModem(int32_t serial, bool on) {
     LOG_CALL << serial;
-    mHal1_5->enableModem(serial, on);
+    mHal1_4->enableModem(serial, on);
     return ok();
 }
 
 ScopedAStatus RadioModem::getBasebandVersion(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getBasebandVersion(serial);
+    mHal1_4->getBasebandVersion(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::getDeviceIdentity(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getDeviceIdentity(serial);
+    mHal1_4->getDeviceIdentity(serial);
     return ok();
 }
 
@@ -58,73 +58,73 @@ ScopedAStatus RadioModem::getImei(int32_t serial) {
 
 ScopedAStatus RadioModem::getHardwareConfig(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getHardwareConfig(serial);
+    mHal1_4->getHardwareConfig(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::getModemActivityInfo(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getModemActivityInfo(serial);
+    mHal1_4->getModemActivityInfo(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::getModemStackStatus(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getModemStackStatus(serial);
+    mHal1_4->getModemStackStatus(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::getRadioCapability(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->getRadioCapability(serial);
+    mHal1_4->getRadioCapability(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::nvReadItem(int32_t serial, aidl::NvItem itemId) {
     LOG_CALL << serial;
-    mHal1_5->nvReadItem(serial, V1_0::NvItem(itemId));
+    mHal1_4->nvReadItem(serial, V1_0::NvItem(itemId));
     return ok();
 }
 
 ScopedAStatus RadioModem::nvResetConfig(int32_t serial, aidl::ResetNvType resetType) {
     LOG_CALL << serial;
-    mHal1_5->nvResetConfig(serial, V1_0::ResetNvType(resetType));
+    mHal1_4->nvResetConfig(serial, V1_0::ResetNvType(resetType));
     return ok();
 }
 
 ScopedAStatus RadioModem::nvWriteCdmaPrl(int32_t serial, const std::vector<uint8_t>& prl) {
     LOG_CALL << serial;
-    mHal1_5->nvWriteCdmaPrl(serial, prl);
+    mHal1_4->nvWriteCdmaPrl(serial, prl);
     return ok();
 }
 
 ScopedAStatus RadioModem::nvWriteItem(int32_t serial, const aidl::NvWriteItem& item) {
     LOG_CALL << serial;
-    mHal1_5->nvWriteItem(serial, toHidl(item));
+    mHal1_4->nvWriteItem(serial, toHidl(item));
     return ok();
 }
 
 ScopedAStatus RadioModem::requestShutdown(int32_t serial) {
     LOG_CALL << serial;
-    mHal1_5->requestShutdown(serial);
+    mHal1_4->requestShutdown(serial);
     return ok();
 }
 
 ScopedAStatus RadioModem::responseAcknowledgement() {
     LOG_CALL;
-    mHal1_5->responseAcknowledgement();
+    mHal1_4->responseAcknowledgement();
     return ok();
 }
 
 ScopedAStatus RadioModem::sendDeviceState(int32_t serial, aidl::DeviceStateType type, bool state) {
     LOG_CALL << serial;
-    mHal1_5->sendDeviceState(serial, V1_0::DeviceStateType(type), state);
+    mHal1_4->sendDeviceState(serial, V1_0::DeviceStateType(type), state);
     return ok();
 }
 
 ScopedAStatus RadioModem::setRadioCapability(int32_t serial, const aidl::RadioCapability& rc) {
     LOG_CALL << serial;
-    mHal1_5->setRadioCapability(serial, toHidl(rc));
+    mHal1_4->setRadioCapability(serial, toHidl(rc));
     return ok();
 }
 
@@ -133,8 +133,10 @@ ScopedAStatus RadioModem::setRadioPower(int32_t serial, bool powerOn, bool forEm
     LOG_CALL << serial;
     if (mHal1_6) {
         mHal1_6->setRadioPower_1_6(serial, powerOn, forEmergencyCall, preferredForEmergencyCall);
-    } else {
+    } else if (mHal1_5) {
         mHal1_5->setRadioPower_1_5(serial, powerOn, forEmergencyCall, preferredForEmergencyCall);
+    } else {
+        mHal1_4->setRadioPower(serial, powerOn);
     }
     return ok();
 }
