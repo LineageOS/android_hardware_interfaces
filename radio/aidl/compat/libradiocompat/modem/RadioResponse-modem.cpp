9a2242ab61cd10bbeb7178322e9aa6c275d997ae
Luofan Chen <clfbbn@gmail.com>
2025-07-30 14:45:40 +0000

    libradiocompat: Deprecate getDeviceIdentity and use getImei instead
    Change-Id: If38b98ad480e4a0e80200276bf73d4a60abf3b26

--- radio/aidl/compat/libradiocompat/modem/RadioResponse-modem.cpp
+++ radio/aidl/compat/libradiocompat/modem/RadioResponse-modem.cpp
@@ -51,9 +51,9 @@ Return<void> RadioResponse::getBasebandVersionResponse(const V1_0::RadioResponse
 
 Return<void> RadioResponse::getDeviceIdentityResponse(  //
         const V1_0::RadioResponseInfo& info, const hidl_string& imei, const hidl_string& imeisv,
-        const hidl_string& esn, const hidl_string& meid) {
+        const hidl_string& /*esn*/, const hidl_string& /*meid*/) {
     LOG_CALL << info.serial;
-    modemCb()->getDeviceIdentityResponse(toAidl(info), imei, imeisv, esn, meid);
+    modemCb()->getImeiResponse(toAidl(info), aidl::ImeiInfo::ImeiType::PRIMARY, imei, imeisv);
     return {};
 }
 
